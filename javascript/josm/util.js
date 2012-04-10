0d85e01551a31581daafb04a6d0c253c1c18c3eb
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-04-10 18:02:28 +0200

    Additional utility functions

--- javascript/josm/util.js
+++ javascript/josm/util.js
@@ -8,6 +8,8 @@ goog.require("josm");
  * @name josm.util
  */
 josm.util = function(my) {
+	// -- imports 
+	var MessageFormat = java.text.MessageFormat;
 	
 	/**
 	 * Checks whether a value is null or undefined.
@@ -21,6 +23,87 @@ josm.util = function(my) {
 	 */
 	my.isNothing = function(value) {
 		return value == null || value == undefined;
+	};
+	
+	/**
+	 * <p>Trims leading and trailing whitespace from <code>s</code>.</p> 
+	 * 
+	 * <p>Replies s, if s is null
+	 * or undefined. Any other value is converted to a string, then leading and trailing white
+	 * space is removed.</p>
+	 * 
+	 * @memberOf josm.util
+	 * @function
+	 * @name trim
+	 * @param {String} s  the string to be trimmed
+	 * @type {String} 
+	 */
+	my.trim = function(s){
+		if (s == null || s == undefined) return s;
+		return String(s).replace(/^\s+|\s+$/, '');
+	};
+	
+	/**
+	 * <p>Assert a condition and throw an Error if the condition isn't met.</p>
+	 * 
+	 * <p><strong>Usage:</strong>
+	 * <dl>
+	 *   <dt><code>assert()</code></dt>
+	 *   <dd>Does nothing</dd>
+	 *   
+	 *    <dt><code>assert(cond)</code></dt>
+	 *    <dd>Checks the condition <code>cond</code>. If it is false, throws an Error.</dd>
+	 *    
+	 *    <dt><code>assert(cond, msg)</code></dt>
+	 *    <dd>Checks the condition <code>cond</code>. If it is false, throws an Error, whose <code>description</code> property
+	 *    is set to <code>msg</code>.</dd>
+     *
+     *	  <dt><code>assert(cond, msg, objs...)</code></dt>
+	 *    <dd>Checks the condition <code>cond</code>. If it is false, throws an Error, whose <code>description</code> property
+	 *    is set to the formatted message <code>msg</code>. Internally uses <code>java.text.MessageFormat</code> to format the message.</dd>
+     *
+	 * </dl>
+	 * 
+	 * @example
+	 * // throws an Error
+	 * assert(false);                  
+	 * 
+	 * // throws an Error e, with e.description == "My message"
+	 * assert(false, "My message");    
+	 * 
+	 * // throws an Error e, with e.description == "My message: test"
+	 * assert(false, "My message: {0}", "test");    
+	 * 
+	 * @memberOf josm.util
+	 * @function
+	 * @name assert
+	 */
+	my.assert = function() {
+		switch(arguments.length) {
+		case 0: 
+			return;
+		case 1:			
+			if (arguments[0]) return;
+			var error = new Error();
+			error.name = "AssertionError";
+			error.description = "An assertion failed";
+			throw error;
+			
+		case 2: 
+			if (arguments[0]) return;
+			var error = new Error();
+		    error.name = "AssertionError";
+		    error.description = arguments[1];
+		    throw error;
+		    
+		default:
+			if (arguments[0]) return;
+		    var error = new Error();
+		    error.name = "AssertionError";
+		    var args = Array.prototype.slice.call(arguments,0)
+		    error.description = MessageFormat.format(arguments[1], args.slice(2));
+		    throw error;
+		}
 	};
 	
 	return my;
