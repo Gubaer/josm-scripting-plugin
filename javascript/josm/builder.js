7e5004fc8683e4783d940499b5e2eb6b8a0ea3ff
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-05-07 18:36:56 +0200

    Renamed, converted into CommonJS module, fixed documentation

--- javascript/josm/builder.js
+++ javascript/josm/builder.js
@@ -1,10 +1,9 @@
-goog.require("josm");
-goog.require("josm.util");
 /**
- * @preserve josm/NodeBuilde.js
- */
-(function() {
-	
+ * Collection of builders for creating OSM nodes, ways and relations.
+ * 
+ * @module josm/builder
+ */	
+
 // -- imports	
 var Node              = org.openstreetmap.josm.data.osm.Node;
 var Way               = org.openstreetmap.josm.data.osm.Way;
@@ -14,26 +13,36 @@ var OsmPrimitiveType  = org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 var SimplePrimitiveId = org.openstreetmap.josm.data.osm.SimplePrimitiveId;
 var LatLon            = org.openstreetmap.josm.data.coor.LatLon;
 
-var util = josm.util;
+var util = require("josm/util");
+
+// -------------------------------------------------------------------------------
+// NodeBuilder
+//--------------------------------------------------------------------------------
+(function() {
 	
 /**
  * <p>NodeBuilder helps to create OSM nodes.</p>
  * 
- * @class NodeBuilder
- * @memberOf josm
+ * @class 
+ * @name NodeBuilder
  * 
  * @example
+ *  var nbuilder = require("josm/builder").NodeBuilder;
+ *  
  *  // create a new local node at position (0,0) without tags
- *  var n1 = josm.NodeBuilder.local();
+ *  var n1 = nbuilder.local();
  *  
  *  // create a new global node at a specific position with tags 
- *  var n2 = josm.NodeBuilder.withPosition(1,1).withTags({name: 'test'}).global(123456);
+ *  var n2 = nbuilder.withPosition(1,1).withTags({name: 'test'}).global(123456);
  *  
  *  // create a new proxy for a global node (an "incomplete" node in JOSM parlance)
- *  var n3 = josm.NodeBuilder.proxy(123456);
+ *  var n3 = nbuilder.proxy(123456);
  */
-josm.NodeBuilder = function() {
-	this.id = void 0;
+exports.NodeBuilder = function(ds) {
+	if (util.isSomething(ds)) {
+		util.assert(ds instanceof DataSet, "Expected a JOSM dataset, got {0}", ds);
+		this.ds = ds;
+	} 
 	this.version = 0;
 	this.lat = 0;
 	this.lon = 0;
@@ -48,28 +57,33 @@ josm.NodeBuilder = function() {
  * <p>The method can be used in a static and in an instance context.</p>
  * 
  * @example
- * // a new proxy node for the global node with id 12345
- * var n1 = josm.NodeBuilder.proxy(12345);  
+ * var nbuilder = require("josm/builder").NodeBuilder;
  * 
- * @function
- * @memberOf josm.NodeBuilder
+ * // a new proxy node for the global node with id 12345
+ * var n1 = nbuilder.proxy(12345);  
+ * 
+ * @memberOf NodeBuilder
+ * @method
  * @return the new proxy node 
- * @name proxy
  * @type org.openstreetmap.josm.data.osm.Node
  */
 function proxy(id) {
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	if (util.isDef(id)) {
-		util.assert(util.isNumber(id) && id !== 0, "Expected a number != 0, got {0}", id);
+		util.assert(util.isNumber(id) && id > 0, "Expected a number > 0, got {0}", id);
 		builder.id = id;
 	}
 	util.assert(util.isNumber(builder.id), "Node id is not a number. Use .proxy(id) or .withId(id).proxy()");
 	util.assert(builder.id > 0, "Expected node id > 0, got {0}", builder.id);
-	return new Node(builder.id);
+	var node = new Node(builder.id);
+	if (builder.ds) {
+		builder.ds.addPrimitive(node);
+	}
+	return node;
 };
-josm.NodeBuilder.proxy = proxy;
-josm.NodeBuilder.prototype.proxy = proxy;
 
+exports.NodeBuilder.proxy = proxy;
+exports.NodeBuilder.prototype.proxy = proxy;
 
 function assignTags(node, tags) {
 	for (var name in tags) {
@@ -95,27 +109,28 @@ function assignNodeAttributes(builder, node) {
  * <p>The method can be used in a static and in an instance context.</p>
  * 
  * @example
+ * var nbuilder = require("josm/builder").NodeBuilder;
+ * 
  * // a new local node with a unique local id
- * var n1 = josm.NodeBuilder.local();  
+ * var n1 = nbuilder.local();  
  * 
  * // a new local node at position (45,23)
- * var n2 = josm.NodeBuilder.withPosition(45,23).local(); 
+ * var n2 = nbuilder.withPosition(45,23).local(); 
  * 
- * @function
- * @memberOf josm.NodeBuilder
+ * @memberOf NodeBuilder
  * @return the new node
- * @name local
  * @type org.openstreetmap.josm.data.osm.Node
  */
 function local() {
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	util.assert(arguments.length == 0, "No arguments expected, got {0} arguments", arguments.length);
 	var node = new Node();
 	assignNodeAttributes(builder, node);
+	if (builder.ds) builder.ds.addPrimitive(node);
 	return node; 
 };
-josm.NodeBuilder.prototype.local = local;
-josm.NodeBuilder.local = local;
+exports.NodeBuilder.prototype.local = local;
+exports.NodeBuilder.local = local;
 
 
 /**
@@ -124,27 +139,27 @@ josm.NodeBuilder.local = local;
  * <p>The method can be used in a static and in an instance context.</p>
  * 
  * @example
+ * var nbuilder = require("josm/builder").NodeBuilder;
+ * 
  * // a new global  node with the global id 12345 at position (0,0) withouth tags
- * var n1 = josm.NodeBuilder.global(12345);  
+ * var n1 = nbuilder.global(12345);  
  * 
  * // a new global node at position (45,23) with id 12345 
- * var n2 = josm.NodeBuilder.withPosition(45,23).withId(12345).global(); 
+ * var n2 = nbuilder.withPosition(45,23).withId(12345).global(); 
  * 
  * // a new global node with id 3456 and version 7 (position is (0,0), the tag <code>name=test</code> is set)
- * var n3 = josm.NodeBuilder.withId(3456,7).withTags({"name":"test"}).global();
+ * var n3 = nbuilder.withId(3456,7).withTags({"name":"test"}).global();
  * 
- * @function
+ * @memberOf NodeBuilder
  * @param {Number} id  (optional) the node id. If supplied, must be a number > 0. If missing, the 
  *   the id must have been declared using <code>.withId(id,version)</code>
  * @param {Number} version (optional) the global node version. If supplied, must be a number >0. If missing,
  *    1 is assumed, unless the version has been set using <code>.withId(id,version)</code>
- * @memberOf josm.NodeBuilder
  * @return the new node
- * @name global
  * @type org.openstreetmap.josm.data.osm.Node
  */
 function global(id, version) {
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	if (util.isDef(id)) {
 		util.assert(util.isNumber(id) && id > 0, "Expected an id > 0, got {0}", id);
 		builder.id = id;
@@ -157,10 +172,11 @@ function global(id, version) {
 	util.assert(util.isDef(builder.version) && builder.version > 0, "Node version not initialized properly. Use .withId(id,version) or .global(id,version). version is {0}", builder.version);
 	var node = new Node(builder.id, builder.version);
 	assignNodeAttributes(builder, node);
+	if (builder.ds) builder.ds.addPrimitive(node);
 	return node;
 };
-josm.NodeBuilder.prototype.global = global;
-josm.NodeBuilder.global = global;
+exports.NodeBuilder.prototype.global = global;
+exports.NodeBuilder.global = global;
 
 /**
  * <p>Declares the node position.</p>
@@ -168,24 +184,22 @@ josm.NodeBuilder.global = global;
  * <p>The method can be used in a static and in an instance context.</p>
  * 
  * @example
+ * var nbuilder = require("josm/builder").NodeBuilder;
+ * 
  * // a new global  node with the global id 12345 at position (34,45) 
- * var n1 = josm.NodeBuilder.withPosition(34,45).global(12345);  
+ * var n1 = nbuilder.withPosition(34,45).global(12345);  
  * 
  * // a new local node at position (23.2, 87.33)
- * with(josm.NodeBuilder) {
- *    var n2 = withPosition(23.3,87.33).local();
- * }
+ * var n2 = nbuilder.withPosition(23.3,87.33).local();
  * 
- * @function
+ * @memberOf NodeBuilder
  * @param {Number} lat  (mandatory) the latitude. A number in the range [-90..90].
  * @param {Number} lon (mandatory) the longitude.  A number in the range [-90..90].
- * @memberOf josm.NodeBuilder
  * @return a node builder (for method chaining)
- * @name withPosition
- * @type josm.NodeBuilder
+ * @type NodeBuilder
  */
 function withPosition(lat, lon){
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	util.assert(util.isNumber(lat), "Expected a number for lat, got {0}", lat);
 	util.assert(util.isNumber(lon), "Expected a number for lon, got {0}", lon);
 	util.assert(LatLon.isValidLat(lat), "Invalid lat, got {0}", lat);
@@ -194,8 +208,8 @@ function withPosition(lat, lon){
 	builder.lon = lon;
 	return builder;
 }
-josm.NodeBuilder.prototype.withPosition = withPosition;
-josm.NodeBuilder.withPosition = withPosition;
+exports.NodeBuilder.prototype.withPosition = withPosition;
+exports.NodeBuilder.withPosition = withPosition;
 
 /**
  * <p>Declares the tags to be assigned to the new node.</p>
@@ -203,27 +217,25 @@ josm.NodeBuilder.withPosition = withPosition;
  * <p>The method can be used in a static and in an instance context.</p>
  * 
  * @example
+ * var nbuilder = require("josm/builder").NodeBuilder;
  * // a new global  node with the global id 12345 and tags name=test and highway=road
- * var n1 = josm.NodeBuilder.withTags({"name":"test", "highway":"road"}).global(12345);  
+ * var n1 = nbuilder.withTags({"name":"test", "highway":"road"}).global(12345);  
  * 
  * // a new local node tags name=test and highway=road
- * with(josm.NodeBuilder) {
- *    var tags = {
+ * var tags = {
  *      "name"    : "test", 
  *      "highway" : "road"
- *    };     
- *    var n2 = withTags(tags).local();
- * }
+ * };     
+ * var n2 = nbuilder.withTags(tags).local();
  * 
- * @function
+ * @memberOf NodeBuilder
  * @param {Object} tags  (optional) the tags 
- * @memberOf josm.NodeBuilder
  * @return a node builder (for method chaining)
  * @name withTags
- * @type josm.NodeBuilder
+ * @type NodeBuilder
  */
 function withTags(tags) {
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	if (util.isNothing(tags)) return this;
 	util.assert(typeof tags === "object", "Expected a hash, got {0}", tags);
 	for (var name in tags) {
@@ -236,8 +248,8 @@ function withTags(tags) {
 	}
 	return builder;
 };
-josm.NodeBuilder.prototype.withTags = withTags;
-josm.NodeBuilder.withTags = withTags;
+exports.NodeBuilder.prototype.withTags = withTags;
+exports.NodeBuilder.withTags = withTags;
 
 
 function assertGlobalId(id) {
@@ -251,19 +263,15 @@ function assertGlobalId(id) {
  * 
  * <p>The method can be used in a static and in an instance context.</p>
  * 
- * @example
- * 
- * @function
+ * @memberOf NodeBuilder
  * @param {Number} id  (mandatory) the global node id. A number > 0.
  * @param {Number} version (optional) the global node version. If present, a number > 0. If missing,
  * the version 1 is assumed.  
- * @memberOf josm.NodeBuilder
  * @return a node builder (for method chaining)
  * @name withId
- * @type josm.NodeBuilder
  */
 function withId(id, version) {
-	var builder = typeof this === "object" ? this : new josm.NodeBuilder();
+	var builder = typeof this === "object" ? this : new exports.NodeBuilder();
 	assertGlobalId(id);	
 	builder.id = id;
 	version = util.isDef(version) ? version: 1;
@@ -272,8 +280,40 @@ function withId(id, version) {
 	builder.version = version; 
 	return builder;
 };
-josm.NodeBuilder.prototype.withId = withId;
-josm.NodeBuilder.withId = withId;
-
+exports.NodeBuilder.prototype.withId = withId;
+exports.NodeBuilder.withId = withId;
 }());
 
+
+//-------------------------------------------------------------------------------
+// WayBuilder
+//--------------------------------------------------------------------------------
+(function() {
+exports.WayBuilder = function() {
+	this.id = void 0;
+	this.version = 0;
+	this.tags = {};
+	this.nodes = [];
+};
+
+function withId(id, version) {
+	
+};
+
+function withNodes() {
+	
+};
+
+function proxy() {
+	
+};
+
+function local() {
+	
+};
+
+function global() {
+	
+};
+	
+}());
