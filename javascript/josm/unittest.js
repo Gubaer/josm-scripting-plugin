060085edecd7cb5e908c24a3ba601df775c3ec97
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-05-27 13:55:53 +0200

    possibility to give a suite a name

--- javascript/josm/unittest.js
+++ javascript/josm/unittest.js
@@ -26,18 +26,30 @@ exports.TestCase.prototype.run = function() {
 	try {
 		this._test();
 		out.println("PASS - " + this._name);
+		return true; 
 	} catch(e) {
 		out.println("FAIL - " + this._name + " - " + e.toSource() + (e.description ? " - " + e.description : ""));
+		return false;
 	}
 };
 
-exports.Suite = function() {
+exports.Suite = function(name) {
+	this._name = name;
 	this._tests = [];	
 };
 
 exports.suite = function() {
-	var suite = new exports.Suite();
-	for(var i = 0; i< arguments.length; i++) {
+	if (arguments.length == 0) new exports.Suite();
+	var idx = 0;	
+	var name = arguments[0];
+	var suite;
+	if (util.isString(name)) {
+		suite = new exports.Suite(name);
+		idx = 1; 
+	} else {
+		suite = new exports.Suite();
+	}
+	for(var i = idx; i< arguments.length; i++) {
 		var test = arguments[i];
 		if (test instanceof exports.TestCase) {
 			suite.add(test);
@@ -54,10 +66,25 @@ exports.Suite.prototype.add = function(test) {
 	this._tests.push(test);
 };
 
-exports.Suite.prototype.run = function() {
-	for (var i = 0; i < this._tests.length; i++) {
-		this._tests[i].run();
+exports.Suite.prototype.run = function() {	
+	out.println("----------------------------------------------------------------------");
+	if (this._name) {
+		out.println("suite: " + this._name);
+		out.println("----------------------------------------------------------------------");
 	}
+	var numtests = 0;
+	var numfail = 0;
+	var numok = 0;
+	for (var i = 0; i < this._tests.length; i++) {
+		var ret = this._tests[i].run();
+		numtests++;
+		if (ret) numok++; else numfail++;
+	}
+	out.println("----------------------------------------------------------------------");
+	out.println(" # tests: " + numtests);
+	out.println(" # PASS : " + numok);
+	out.println(" # FAIL : " + numfail);
+	out.println("----------------------------------------------------------------------");
 };
 
 
@@ -71,12 +98,24 @@ exports.expectError = function(f) {
 };
 
 exports.expectAssertionError = function(f) {
+	var name = "no name";
+	var f;
+	switch(arguments.length) {
+	case 0: return;
+	case 1: f = arguments[0]; break;
+	case 2:
+		name = arguments[0]; 
+		f = arguments[1];
+		break;
+	default:
+		util.assert(false, "Unexpected number of arguments");
+	}
 	try {
-		f();
-		util.assert(false, "Expected an error, didn't get one.");
+		f();		
+		util.assert(false, "''{0}'': should have failed. Didn't catch an error.", name);
 	} catch(e) {
 		if (e.name != "AssertionError") {
-			util.assert(false, "Expected AssertionError, got {0}", e.toSource());
+			util.assert(false, "''{0}'': expected an AssertionError, caught {1}.", name, e.toSource());
 		} 
 	}
 };
