576e31db93d3e795cadf10ce56f1d0f0ea325ec3
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-05-28 17:49:02 +0200

    test cases for NodeMixin, bug fixes

--- test/script-api/josm/NodeMixinTest.js
+++ test/script-api/josm/NodeMixinTest.js
@@ -6,6 +6,7 @@ var nb = require("josm/builder").NodeBuilder;
 var DataSet = org.openstreetmap.josm.data.osm.DataSet;
 var Node    = org.openstreetmap.josm.data.osm.Node;
 var Way     = org.openstreetmap.josm.data.osm.Way;
+var LatLon     = org.openstreetmap.josm.data.coor.LatLon;
 var ArrayList = java.util.ArrayList;
 
 tu.suite("properties access",
@@ -86,6 +87,118 @@ tu.suite("properties access",
 		tu.expectAssertionError("can't set lon on a proxy node", function() {
 			n = nb.createProxy(12345);
 			n.lon = 23.44;
+		});
+	}),
+	
+	test("pos - get/set ", function() {
+		var n;
+
+		// a global node
+		n = nb.create(1234);
+		n.pos = new LatLon(1,2);
+		var pos = n.pos;
+		util.assert(pos.lat() == 1, "1 - unexpected lat");
+		util.assert(pos.lon() == 2, "1 - unexpected lon");
+		
+		// a local node
+		n = nb.create();
+		n.pos = new LatLon(1,2);
+		var pos = n.pos;
+		util.assert(pos.lat() == 1, "2 - unexpected lat");
+		util.assert(pos.lon() == 2, "2 - unexpected lon");
+		
+		// a loc
+		n = nb.create();
+		n.pos = {lat: 1, lon: 2};
+		util.assert(n.lat == 1, "3 - unexpected lat");
+		util.assert(n.lon == 2, "3 - unexpected lon");
+		
+			
+		tu.expectAssertionError("can't assign null", function() {
+			n.pos = null;
+		});
+		tu.expectAssertionError("can't assign undefined", function() {
+			n.pos = undefined;
+		});
+		tu.expectAssertionError("can't assign a string", function() {
+			n.pos= "1,2";
+		});
+
+		tu.expectAssertionError("can't assign an object with missing lat property", function() {
+			n.lon = {lon: 123};
+		});
+		
+		tu.expectAssertionError("can't assign an object with missing lon property", function() {
+			n.lon = {lat: 2};
+		});
+		tu.expectAssertionError("can't assign an object wit illegal lat - null", function() {
+			n.lon = {lat: null, lon: 2};
+		});
+		tu.expectAssertionError("can't assign an object wit illegal lat - undefined", function() {
+			n.lon = {lat: undefined, lon: 2};
+		});
+		tu.expectAssertionError("can't assign an object wit illegal lat - string", function() {
+			n.lon = {lat: "1", lon: 2};
+		});
+		tu.expectAssertionError("can't assign an object wit illegal lat - illegal value", function() {
+			n.lon = {lat: -456, lon: 2};
+		});
+		tu.expectAssertionError("can't assign an object with illegal lon - null", function() {
+			n.lon = {lat: 1, lon: null};
+		});
+		tu.expectAssertionError("can't assign an object with illegal lon - undefined", function() {
+			n.lon = {lat: 1, lon: undefined};
+		});
+		tu.expectAssertionError("can't assign an object with illegal lon - string", function() {
+			n.lon = {lat: 1, lon: "2"};
+		});
+		tu.expectAssertionError("can't assign an object with illegal lon - illegal value", function() {
+			n.lon = {lat: 1, lon: 564};
+		});
+	}),
+	
+	test("east - set/get", function() {
+		var n;
+
+		// should have a defined east
+		n = nb.create(1234);
+		n.pos = new LatLon(1,2);
+		util.assert(util.isNumber(n.east), "1- east should be a number");
+		
+		// proxies don't have an east
+		n = nb.createProxy(1234);
+		util.assert(n.east == undefined, "east should be undefined");
+		
+		// local nodes should have defined east (assuming 
+		// the current projection is set)
+		n = nb.create();
+		util.assert(util.isNumber(n.east), "2 - east should be a number");
+		
+		tu.expectError("can't set value for 'east'", function() {
+			n.east = 1;
+		});
+		
+	}),
+	
+	test("north - set/get", function() {
+		var n;
+
+		// should have a defined east
+		n = nb.create(1234);
+		n.pos = new LatLon(1,2);
+		util.assert(util.isNumber(n.north), "1-  north should be a number");
+		
+		// proxies don't have an east
+		n = nb.createProxy(1234);
+		util.assert(n.north == undefined, "north should be undefined");
+		
+		// local nodes should have defined north (assuming 
+		// the current projection is set)
+		n = nb.create();
+		util.assert(util.isNumber(n.north), "2 - north should be a number");
+		
+		tu.expectError("can't set value for 'north'", function() {
+			n.north = 1;
 		});
 	})
 ).run();
