b61ade3ce144ab784dd509ae9c62f7500cea2f36
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-05-30 12:34:22 +0200

    remember last opened directory in preferences

--- src/org/openstreetmap/josm/plugins/scripting/ui/console/OpenAction.java
+++ src/org/openstreetmap/josm/plugins/scripting/ui/console/OpenAction.java
@@ -9,19 +9,40 @@ import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 
+import org.openstreetmap.josm.data.preferences.StringProperty;
 import org.openstreetmap.josm.tools.ImageProvider;
 
 public class OpenAction extends AbstractAction {
+	private static final long serialVersionUID = 1L;
 	static private final Logger logger = Logger.getLogger(OpenAction.class.getName());
+	static public final StringProperty PREF_LAST_OPEN_DIR = new StringProperty(
+			OpenAction.class.getName() + ".lastDir", 
+			null
+	 );
+	
+	protected File getLastOpenedDirectory() {
+		String dir = PREF_LAST_OPEN_DIR.get();
+		if (dir == null) return null;
+		File f = new File(dir);
+		if (! f.isDirectory() && f.canRead()) return null;
+		return f;
+	}
 	
 	protected File askFile() {
 		JFileChooser chooser = new JFileChooser();
 		chooser.setDialogTitle(tr("Select a script"));
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		chooser.setMultiSelectionEnabled(false);
+		chooser.setMultiSelectionEnabled(false);		
+		File dir = getLastOpenedDirectory();
+		if (dir != null) {
+			chooser.setCurrentDirectory(dir);
+		}
 		int ret = chooser.showOpenDialog(ScriptingConsole.getScriptingConsole());			
-		if (ret != JFileChooser.APPROVE_OPTION) return null;
-		
+		if (ret != JFileChooser.APPROVE_OPTION) return null;		
+		dir = chooser.getCurrentDirectory();
+		if (dir != null){
+			PREF_LAST_OPEN_DIR.put(dir.getAbsolutePath());
+		}
 		return chooser.getSelectedFile();
 	}
 	
