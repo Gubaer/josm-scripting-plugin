1042f7b37a620cd28ac2e833fb88e045d2609ddc
Karl Guggisberg <karl.guggisberg@guggis.ch>
2012-07-08 12:04:49 +0200

    Documentation of alias names for properties

--- doc/template/ForClassTagPlugin.js
+++ doc/template/ForClassTagPlugin.js
@@ -4,14 +4,22 @@
  * 
  * See https://github.com/jsdoc3/jsdoc/tree/master/plugins
  */
-java.lang.System.out.println("ForClassTagPlugin: loading ...");
+java.lang.System.out.println("CustomTagsPlugin: loading ...");
 exports.defineTags = function(dictionary) {
-	java.lang.System.out.println("ForClassTagPlugin: defining tag 'forclass' ...");
+	java.lang.System.out.println("CustomTagsPlugin: defining tag 'forclass' ...");
 	dictionary.defineTag('forclass', {
 		mustHaveValue: true,
 	    onTagged: function(doclet, tag) {
 	    	// just remember the tag
 	        doclet.forclass = tag.value; 
+	    }
+	});
+	java.lang.System.out.println("CustomTagsPlugin: defining tag 'alias' ...");
+	dictionary.defineTag('alias', {
+		mustHaveValue: true,
+	    onTagged: function(doclet, tag) {
+	    	doclet.alias = doclet.alias || [];
+	    	doclet.alias.push(tag.value);
 	    }
 	});
 };
