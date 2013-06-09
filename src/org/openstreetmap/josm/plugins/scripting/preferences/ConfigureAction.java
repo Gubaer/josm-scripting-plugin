f4ae15fd997b798f3b1be89203c3a055291a1d43
Karl Guggisberg <karl.guggisberg@guggis.ch>
2013-06-09 11:32:57 +0200

    fixed #11

--- src/org/openstreetmap/josm/plugins/scripting/preferences/ConfigureAction.java
+++ src/org/openstreetmap/josm/plugins/scripting/preferences/ConfigureAction.java
@@ -3,28 +3,56 @@ package org.openstreetmap.josm.plugins.scripting.preferences;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.gui.help.HelpUtil;
 import org.openstreetmap.josm.gui.preferences.PreferenceDialog;
 
+@SuppressWarnings("serial")
 public class ConfigureAction extends JosmAction {
+    //static private Logger logger = Logger.getLogger(
+    //            ConfigureAction.class.getName());
+
+    private PreferenceDialog dialog;
+
 	public ConfigureAction() {
 		super(
 			tr("Configure..."),        			    // title
 			"scripting-preference", 			    // icon name
-			tr("Configure scripting preferences"),  // tooltip 
-			null,                                   // no shortcut 
+			tr("Configure scripting preferences"),  // tooltip
+			null,                                   // no shortcut
 			false                                   // don't register
-		);		
+		);
 		putValue("help", HelpUtil.ht("/Plugin/Scripting"));
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent evt) {
-		PreferenceDialog dialog = new PreferenceDialog(Main.parent);
-		dialog.selectPreferencesTabByName(PreferenceEditor.ICON_NAME);	
+		dialog = new PreferenceDialog(Main.parent);
+		dialog.addComponentListener(visibilityListener);
 		dialog.setVisible(true);
-	}		
+	}
+
+    private final ComponentListener visibilityListener = new  ComponentListener() {
+        @Override
+        public void componentHidden(ComponentEvent arg0) {
+            // cleanup when dialog becomes invisible
+            dialog.removeComponentListener(this);
+            dialog = null;
+        }
+        @Override
+        public void componentShown(ComponentEvent arg0) {
+            // programatically select scripting preferences tab when
+            // dialog becomes visible
+            dialog.selectPreferencesTabByName(PreferenceEditor.ICON_NAME);
+        }
+
+        @Override
+        public void componentMoved(ComponentEvent arg0) {/* ignore */}
+        @Override
+        public void componentResized(ComponentEvent arg0) {/* ignore */}
+    };
 }
