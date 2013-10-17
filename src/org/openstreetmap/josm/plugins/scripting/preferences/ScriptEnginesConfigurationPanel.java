62326447aaf2639f7c97da5f823d07b88f9a66f7
Karl Guggisberg <karl.guggisberg@guggis.ch>
2013-10-17 20:33:09 +0200

    cosmetics, spacing

--- src/org/openstreetmap/josm/plugins/scripting/preferences/ScriptEnginesConfigurationPanel.java
+++ src/org/openstreetmap/josm/plugins/scripting/preferences/ScriptEnginesConfigurationPanel.java
@@ -47,21 +47,22 @@ import org.openstreetmap.josm.plugins.scripting.ui.ScriptEngineCellRenderer;
 import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
- * <p><strong>ScriptEnginesConfigurationPanel</strong> allows to configure 
+ * <p><strong>ScriptEnginesConfigurationPanel</strong> allows to configure
  * the script engines available in JOSM.</p>
- * 
+ *
  */
+@SuppressWarnings("serial")
 public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
-	
+
 	private ScriptEngineJarTableModel model;
 	private JTable tblJarFiles;
 	private RemoveJarAction actDelete;
-	
+
 	public ScriptEnginesConfigurationPanel() {
 		build();
 		model.restoreFromPreferences();
 	}
-	
+
 	protected JPanel buildScriptEnginesInfoPanel() {
 		JPanel pnl = new JPanel(new BorderLayout());
 		JList lstEngines = new JList(JSR223ScriptEngineProvider.getInstance());
@@ -71,45 +72,45 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 		lstEngines.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		return pnl;
 	}
-	
+
 	protected JPanel buildScriptEngineJarsPanel() {
 		JPanel pnl = new JPanel(new GridBagLayout());
 		pnl.setBorder(
-				BorderFactory.createCompoundBorder(
-						BorderFactory.createEmptyBorder(3,3,3,3),
-						BorderFactory.createTitledBorder(tr("JAR files"))
-				)
+			BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(3,3,3,3),
+				BorderFactory.createTitledBorder(tr("JAR files"))
+			)
 		);
 
 		GridBagConstraints gc = new GridBagConstraints();
-		
+
 		HtmlPanel info = new HtmlPanel();
 		info.setText(
-				"<html>"
-				+ tr("Enter additional JAR files which provide script engines.")
-				+ "</html>"
+			"<html>"
+			+ tr("Enter additional JAR files which provide script engines.")
+			+ "</html>"
 		);
 		gc.gridx = 0; gc.gridy = 0;
 		gc.weightx = 1.0; gc.weighty = 0.0;
 		gc.fill = GridBagConstraints.BOTH;
 		pnl.add(info, gc);
-		
+
 		model = new ScriptEngineJarTableModel();
 		tblJarFiles= new JTable(model, new ColumnModel());
 		tblJarFiles.setSelectionModel(model.getSelectionModel());
 		tblJarFiles.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
 		tblJarFiles.setRowHeight(new JButton("...").getPreferredSize().height);
-		
+
 		JScrollPane jp = new JScrollPane(tblJarFiles);
 		jp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		jp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		jp.setMinimumSize(new Dimension(0, 100));
-		
+
 		gc.gridx = 0; gc.gridy = 1;
 		gc.weightx = 1.0; gc.weighty = 1.0;
 		gc.fill = GridBagConstraints.BOTH;
 		pnl.add(jp, gc);
-	
+
 		JPanel ctrlPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		AddJarAction actAdd = new AddJarAction();
 		ctrlPanel.add(new JButton(actAdd));
@@ -117,31 +118,33 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 		ctrlPanel.add(new JButton(actDelete));
 		model.getSelectionModel().addListSelectionListener(actDelete);
 		tblJarFiles.getActionMap().put("deleteSelection", actDelete);
-		tblJarFiles.getInputMap(WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0),"deleteSelection");
+		tblJarFiles.getInputMap(WHEN_FOCUSED).put(KeyStroke.getKeyStroke(
+		        KeyEvent.VK_DELETE, 0),"deleteSelection");
 		tblJarFiles.getActionMap().put("insertRow", actAdd);
-		tblJarFiles.getInputMap(WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_INSERT, 0),"insertRow");
+		tblJarFiles.getInputMap(WHEN_FOCUSED).put(KeyStroke.getKeyStroke(
+		        KeyEvent.VK_INSERT, 0),"insertRow");
 		gc.gridx = 0; gc.gridy = 2;
 		gc.weightx = 1.0; gc.weighty = 0.0;
 		gc.fill = GridBagConstraints.HORIZONTAL;
 		pnl.add(ctrlPanel, gc);
 		return pnl;
 	}
-	
+
 	protected JPanel buildScriptEnginesPanel() {
-		JPanel pnl = new JPanel(new GridBagLayout());	
+		JPanel pnl = new JPanel(new GridBagLayout());
 		pnl.setBorder(
-				BorderFactory.createCompoundBorder(
-						BorderFactory.createEmptyBorder(3,3,3,3),
-						BorderFactory.createTitledBorder(tr("Available script engines"))
-				)
+			BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(3,3,3,3),
+				BorderFactory.createTitledBorder(tr("Available script engines"))
+			)
 		);
 		GridBagConstraints gc = new GridBagConstraints();
-		
+
 		HtmlPanel info = new HtmlPanel();
 		info.setText(
-				"<html>"
-				+ tr("JOSM currently supports the following script engines:")
-				+ "</html>"
+			"<html>"
+			+ tr("JOSM currently supports the following script engines:")
+			+ "</html>"
 		);
 		gc.gridx = 0; gc.gridy = 0;
 		gc.weightx = 1.0; gc.weighty = 0.0;
@@ -156,7 +159,7 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 		pnl.add(buildScriptEnginesInfoPanel(), gc);
 		return pnl;
 	}
-	
+
 	protected void build() {
 		setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
@@ -164,19 +167,19 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 		gc.weightx = 1.0; gc.weighty = 0.0;
 		gc.fill = GridBagConstraints.HORIZONTAL;
 		add(buildScriptEnginesPanel(), gc);
-		
+
 		gc.gridx = 0; gc.gridy = 1;
 		gc.weightx = 1.0; gc.weighty = 0.0;
 		gc.fill = GridBagConstraints.HORIZONTAL;
 		add(buildScriptEngineJarsPanel(), gc);
 
-		// filler 
+		// filler
 		gc.gridx = 0; gc.gridy = 2;
 		gc.weightx = 1.0; gc.weighty = 1.0;
 		gc.fill = GridBagConstraints.BOTH;
 		add(new JPanel(), gc);
 	}
-	
+
 	public void persistToPreferences() {
 		model.persistToPreferences();
 	}
@@ -184,36 +187,37 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 	public void restoreFromPreferences() {
 		model.restoreFromPreferences();
 	}
-	
-	static private class ColumnModel extends DefaultTableColumnModel {		
+
+	static private class ColumnModel extends DefaultTableColumnModel {
 		public ColumnModel() {
 			TableColumn tc;
-			ScriptEngineJarCellRenderer renderer = new ScriptEngineJarCellRenderer();
-			
+			ScriptEngineJarCellRenderer renderer =
+			    new ScriptEngineJarCellRenderer();
+
 			tc = new TableColumn(0);
 			tc.setHeaderValue("");
 			tc.setMaxWidth(30);
 			tc.setPreferredWidth(30);
 			tc.setMinWidth(30);
 			tc.setResizable(false);
-			tc.setCellRenderer(renderer);			
+			tc.setCellRenderer(renderer);
 			addColumn(tc);
-			
+
 			tc = new TableColumn(1);
 			tc.setHeaderValue(tr("JAR file"));
 			tc.setCellRenderer(renderer);
 			tc.setCellEditor(new JarFileNameEditor());
-			tc.setResizable(true);			
+			tc.setResizable(true);
 			addColumn(tc);
 		}
 	}
-	
+
 	static private class ScriptEngineJarCellRenderer extends JLabel implements TableCellRenderer {
 
 		public ScriptEngineJarCellRenderer(){
 			setOpaque(true);
 		}
-		
+
 		protected void reset() {
 			setIcon(null);
 			setText("");
@@ -221,7 +225,7 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 			setBackground(UIManager.getColor("Table.background"));
 			setHorizontalAlignment(SwingConstants.LEFT);
 		}
-		
+
 		protected void renderColors(boolean selected){
 			if (!selected){
 				setForeground(UIManager.getColor("Table.foreground"));
@@ -231,7 +235,7 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 				setBackground(UIManager.getColor("Table.selectionBackground"));
 			}
 		}
-		
+
 		protected void renderJarName(ScriptEngineJarInfo jar) {
 			String fileName = jar.getJarFilePath();
 			File f = new File(fileName.trim());
@@ -239,7 +243,8 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 			if (parent != null){
 				String parentName= parent.getName();
 				if (parentName.length() > 15){
-					setText(parentName.substring(0, 10) + "..."  + File.pathSeparator + f.getName());										
+					setText(parentName.substring(0, 10) + "..."
+					        + File.pathSeparator + f.getName());
 				} else {
 					setText(f.toString());
 				}
@@ -247,7 +252,7 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 				setText(f.toString());
 			}
 		}
-		
+
 		protected void renderJarStatus(ScriptEngineJarInfo jar){
 			setHorizontalAlignment(SwingConstants.CENTER);
 			String msg = jar.getStatusMessage();
@@ -262,74 +267,79 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 				setToolTipText(msg);
 			}
 		}
-		
+
 		@Override
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		public Component getTableCellRendererComponent(JTable table,
+		        Object value, boolean isSelected, boolean hasFocus,
+		        int row, int column) {
 			ScriptEngineJarInfo jar = (ScriptEngineJarInfo)value;
 			reset();
 			switch(column){
 			case 0: renderJarStatus(jar); break;
 			case 1:
 				renderColors(isSelected);
-				renderJarName(jar); 
+				renderJarName(jar);
 				break;
 			}
 			return this;
-		}		
+		}
 	}
-	
-	private class RemoveJarAction extends AbstractAction implements ListSelectionListener{
+
+	private class RemoveJarAction extends AbstractAction
+	    implements ListSelectionListener{
 
 		public RemoveJarAction() {
 			putValue(NAME, tr("Remove"));
 			putValue(SHORT_DESCRIPTION, tr("Remove the selected jar files"));
 			putValue(SMALL_ICON, ImageProvider.get("dialogs","delete"));
 			updateEnabledState();
-		}		
-		
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			model.deleteSelected();			
+			model.deleteSelected();
 		}
-		
+
 		public void updateEnabledState() {
 			setEnabled(!model.getSelectionModel().isSelectionEmpty());
 		}
 
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
-			updateEnabledState();			
+			updateEnabledState();
 		}
 	}
-	
+
 	private class AddJarAction extends AbstractAction {
 
 		public AddJarAction() {
 			putValue(NAME, tr("Add"));
-			putValue(SHORT_DESCRIPTION, tr("Add a jar file providing a script engine"));
+			putValue(SHORT_DESCRIPTION,
+			    tr("Add a jar file providing a script engine"));
 			putValue(SMALL_ICON, ImageProvider.get("add"));
-		}		
-		
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			model.addNew();		
+			model.addNew();
 			tblJarFiles.editCellAt(tblJarFiles.getRowCount()-1, 1);
 		}
 	}
-	
+
 	private static class JarFileNameEditor extends JPanel implements TableCellEditor {
-		static private final Logger logger = Logger.getLogger(JarFileNameEditor.class.getName());
+		static private final Logger logger = Logger.getLogger(
+		    JarFileNameEditor.class.getName());
 
 		private JTextField tfJarFile;
 		private JButton btnLauchFileChooser;
 		private TableCellEditorSupport tableCellEditorSupport;
 		private ScriptEngineJarInfo info;
-		
+
 		public JarFileNameEditor() {
 			tableCellEditorSupport = new TableCellEditorSupport(this);
 			build();
 		}
-		
+
 		protected void build() {
 			setLayout(new GridBagLayout());
 			GridBagConstraints gc = new GridBagConstraints();
@@ -338,12 +348,14 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 			gc.fill = GridBagConstraints.BOTH;
 			add(tfJarFile = new JTextField(), gc);
 			SelectAllOnFocusGainedDecorator.decorate(tfJarFile);
-			
+
 			gc.gridx = 1; gc.gridy = 0;
 			gc.weightx = 0.0; gc.weighty = 1.0;
 			gc.fill = GridBagConstraints.VERTICAL;
-			add(btnLauchFileChooser = new JButton(new LaunchFileChooserAction()), gc);
-			
+			add(btnLauchFileChooser = new JButton(
+			    new LaunchFileChooserAction()), gc
+			);
+
 		}
 
 		public void addCellEditorListener(CellEditorListener l) {
@@ -380,19 +392,20 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 		}
 
 		@Override
-		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+		public Component getTableCellEditorComponent(JTable table,
+		    Object value, boolean isSelected, int row, int column) {
 			info = (ScriptEngineJarInfo)value;
 			tfJarFile.setText(info.getJarFilePath());
 			tfJarFile.selectAll();
 			return this;
 		}
-		
+
 		private class LaunchFileChooserAction extends AbstractAction {
 			public LaunchFileChooserAction() {
 				putValue(NAME, "...");
-				putValue(SHORT_DESCRIPTION, tr("Launch file chooser"));				
+				putValue(SHORT_DESCRIPTION, tr("Launch file chooser"));
 			}
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				String fileName = tfJarFile.getText().trim();
@@ -408,16 +421,15 @@ public class ScriptEnginesConfigurationPanel extends VerticallyScrollablePanel{
 					chooser.setCurrentDirectory(currentFile);
 					chooser.setSelectedFile(currentFile);
 				}
-				int ret = chooser.showOpenDialog(btnLauchFileChooser);			
-				if (ret == JFileChooser.APPROVE_OPTION) {				
+				int ret = chooser.showOpenDialog(btnLauchFileChooser);
+				if (ret == JFileChooser.APPROVE_OPTION) {
 					currentFile = chooser.getSelectedFile();
-					tfJarFile.setText(currentFile.toString());		
+					tfJarFile.setText(currentFile.toString());
 					stopCellEditing();
 				} else {
-					tfJarFile.requestFocusInWindow();					
+					tfJarFile.requestFocusInWindow();
 				}
 			}
 		}
-		
 	}
 }
