995e20469f098da20c7eea2a8e0958b77696b4ae
Gubaer <karl.guggisberg@guggis.ch>
2016-08-16 10:14:34 +0200

    reformat

--- src/org/openstreetmap/josm/plugins/scripting/model/PreferenceKeys.java
+++ src/org/openstreetmap/josm/plugins/scripting/model/PreferenceKeys.java
@@ -5,8 +5,8 @@ package org.openstreetmap.josm.plugins.scripting.model;
  */
 public interface PreferenceKeys {
     /**
-     * <p>Preference entry for a list of jar files (full path per file) providing
-     * JSR 223 compatible scripting engines.</p>
+     * <p>Preference entry for a list of jar files (full path per file)
+     * providing JSR 223 compatible scripting engines.</p>
      *
      * <p><strong>Default:</strong></p> - empty collection
      */
@@ -18,34 +18,38 @@ public interface PreferenceKeys {
     String PREF_KEY_FILE_HISTORY = "scripting.RunScriptDialog.file-history";
 
     /**
-     * <p>The preferences key for the last script file name entered in the script file
-     * selection field.</p>
+     * <p>The preferences key for the last script file name entered in the
+     * script file selection field.</p>
      */
     String PREF_KEY_LAST_FILE = "scripting.RunScriptDialog.last-file";
 
 
     /**
-     * <p>The preferences key for the current default engine used in the scripting
-     * console. The value is string <em>type/id</em>, where <code>type</code> is
-     * either <code>embedded</code> or <code>plugged</code>.</p>
+     * <p>The preferences key for the current default engine used in the
+     * scripting console. The value is string <em>type/id</em>, where
+     * <code>type</code> is either <code>embedded</code> or
+     * <code>plugged</code>.</p>
      *
-     * <p>If type is <code>plugged</code>, the the is the name of a JSR 223 compliant
-     * scripting engine. It is is <code>embedded</code> then the id is the name of an
-     * embedded scripting engine (currently always <code>rhino</code>).</p>
+     * <p>If type is <code>plugged</code>, the the is the name of a JSR 223
+     * compliant scripting engine. It is is <code>embedded</code> then the id
+     * is the name of an embedded scripting engine (currently always
+     * <code>rhino</code>).</p>
      *
-     * <p>If missing or invalid, the embedded default scripting engine is assumed.</p>
+     * <p>If missing or invalid, the embedded default scripting engine is
+     * assumed.</p>
      *
      */
     String PREF_KEY_SCRIPTING_ENGINE = "scripting.console.default.engine";
 
 
     /**
-     * <p>The array of CommonJS module repositories. Entries are URLs, either file
-     *   URLs or jar URLs.</p>
+     * <p>The array of CommonJS module repositories. Entries are URLs,
+     * either file URLs or jar URLs.</p>
      *
      * <p>Default value: empty</p>
      */
-    String PREF_KEY_COMMONJS_MODULE_REPOSITORIES = "scripting.commonjs-module-repositories";
+    String PREF_KEY_COMMONJS_MODULE_REPOSITORIES =
+            "scripting.commonjs-module-repositories";
 
     /**
      * <p>The array of local paths which are added to the internal to
