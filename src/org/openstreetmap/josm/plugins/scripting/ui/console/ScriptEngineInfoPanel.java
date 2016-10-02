67d27858aded4d14d2bd464719f04bd8f67cdecc
Gubaer <karl.guggisberg@guggis.ch>
2016-10-02 16:57:09 +0200

    reformat

--- src/org/openstreetmap/josm/plugins/scripting/ui/console/ScriptEngineInfoPanel.java
+++ src/org/openstreetmap/josm/plugins/scripting/ui/console/ScriptEngineInfoPanel.java
@@ -16,6 +16,7 @@ import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.StyleSheet;
+import javax.validation.constraints.NotNull;
 
 import org.openstreetmap.josm.plugins.scripting.model.ScriptEngineDescriptor;
 import org.openstreetmap.josm.plugins.scripting.ui.ScriptEngineSelectionDialog;
@@ -23,10 +24,12 @@ import org.openstreetmap.josm.plugins.scripting.util.Assert;
 
 /**
  * Displays summary information about the currently selected scripting engine.
- *
  */
-public class ScriptEngineInfoPanel extends JPanel implements PropertyChangeListener, HyperlinkListener{
-    static private final Logger logger = Logger.getLogger(ScriptEngineInfoPanel.class.getName());
+public class ScriptEngineInfoPanel extends JPanel implements
+PropertyChangeListener, HyperlinkListener{
+    @SuppressWarnings("unused")
+    static private final Logger logger =
+            Logger.getLogger(ScriptEngineInfoPanel.class.getName());
 
     private JEditorPane jepInfo;
     private ScriptEditorModel model;
@@ -36,8 +39,8 @@ public class ScriptEngineInfoPanel extends JPanel implements PropertyChangeListe
      *
      * @param model the model to listen too for updated script engines
      */
-    public ScriptEngineInfoPanel(ScriptEditorModel model){
-        Assert.assertArgNotNull(model, "model");
+    public ScriptEngineInfoPanel(@NotNull ScriptEditorModel model){
+        Assert.assertArgNotNull(model);
         model.addPropertyChangeListener(this);
         this.model = model;
         build();
@@ -49,16 +52,23 @@ public class ScriptEngineInfoPanel extends JPanel implements PropertyChangeListe
         jepInfo.setOpaque(false);
         jepInfo.setEditable(false);
         jepInfo.addHyperlinkListener(this);
-        Font f = UIManager.getFont("Label.font");
-        StyleSheet ss = new StyleSheet();
+        final Font f = UIManager.getFont("Label.font");
+        final StyleSheet ss = new StyleSheet();
+        final String cssRuleFontFamily =
+            "font-family: ''{0}'';font-size: {1,number}pt; font-weight: {2}; "
+                + "font-style: {3}";
         String rule = MessageFormat
-                .format("font-family: ''{0}'';font-size: {1,number}pt; font-weight: {2}; font-style: {3}",
-                        f.getName(), f.getSize(), f.isBold() ? "bold"
-                                : "normal", f.isItalic() ? "italic" : "normal");
+                .format(cssRuleFontFamily,
+                        f.getName(), f.getSize(),
+                        f.isBold() ? "bold" : "normal",
+                        f.isItalic() ? "italic" : "normal");
         rule = "body {" + rule + "}";
+        ss.addRule(rule);
         rule = MessageFormat
-                .format("font-family: ''{0}'';font-size: {1,number}pt; font-weight: {2}; font-style: {3}",
-                        f.getName(), f.getSize(), "bold",
+                .format(cssRuleFontFamily,
+                        f.getName(),
+                        f.getSize(),
+                        "bold",
                         f.isItalic() ? "italic" : "normal");
         rule = "strong {" + rule + "}";
         ss.addRule(rule);
@@ -79,53 +89,64 @@ public class ScriptEngineInfoPanel extends JPanel implements PropertyChangeListe
         if (desc == null){
             sb.append("<html>");
             sb.append(tr("No script engine selected.")).append(" ");
-            sb.append("<a href=\"urn:select-script-engine\">").append(tr("Select...")).append("</a>");
+            sb.append("<a href=\"urn:select-script-engine\">")
+                .append(tr("Select...")).append("</a>");
             sb.append("<html>");
         } else if (desc.isDefault()) {
             sb.append("<html>");
             sb.append(
-                    tr("Executing scripts with the built-in scripting engine for <strong>{0}</strong> based on <strong>{1}</strong>.",
+                    tr("Executing scripts with the built-in scripting engine "
+                    +"for <strong>{0}</strong> based on <strong>{1}</strong>.",
                     desc.getLanguageName(),
                     desc.getEngineName()
                     )
             );
             sb.append(" ");
-            sb.append("<a href=\"urn:change-script-engine\">").append(tr("Change...")).append("</a>");
+            sb.append("<a href=\"urn:change-script-engine\">")
+                .append(tr("Change...")).append("</a>");
             sb.append("</html>");
         } else {
             sb.append("<html>");
             sb.append(tr(
-                    "Executing scripts in language <strong>{0}</strong> using engine <strong>{1}</strong>.",
-                    desc.getLanguageName() == null ? tr("unknown") : desc.getLanguageName(),
-                    desc.getEngineName() == null ? tr("unknown") : desc.getEngineName()
+                    "Executing scripts in language <strong>{0}</strong> "
+                    + "using engine <strong>{1}</strong>.",
+                    desc.getLanguageName() == null ?
+                            tr("unknown") : desc.getLanguageName(),
+                    desc.getEngineName() == null ?
+                            tr("unknown") : desc.getEngineName()
                 )
             );
             sb.append(" ");
-            sb.append("<a href=\"urn:change-script-engine\">").append(tr("Change...")).append("</a>");
+            sb.append("<a href=\"urn:change-script-engine\">")
+                .append(tr("Change...")).append("</a>");
             sb.append("</html>");
         }
         jepInfo.setText(sb.toString());
     }
 
     protected void promptForScriptEngine() {
-        ScriptEngineDescriptor desc = ScriptEngineSelectionDialog.select(this, model.getScriptEngineDescriptor());
+        ScriptEngineDescriptor desc = ScriptEngineSelectionDialog.select(
+                this, model.getScriptEngineDescriptor());
         if (desc != null){
             model.setScriptEngineDescriptor(desc);
         }
     }
 
-    /* ----------------------------------------------------------------------- */
-    /* interface PropertyChagneListener                                        */
-    /* ----------------------------------------------------------------------- */
+    /* --------------------------------------------------------------------- */
+    /* interface PropertyChagneListener                                      */
+    /* --------------------------------------------------------------------- */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (!evt.getPropertyName().equals(ScriptEditorModel.PROP_SCRIPT_ENGINE)) return;
+        if (!evt.getPropertyName().equals(
+                ScriptEditorModel.PROP_SCRIPT_ENGINE)) {
+            return;
+        }
         refreshInfo((ScriptEngineDescriptor)evt.getNewValue());
     }
 
-    /* ----------------------------------------------------------------------- */
-    /* interface HyperlinkListener                                             */
-    /* ----------------------------------------------------------------------- */
+    /* --------------------------------------------------------------------- */
+    /* interface HyperlinkListener                                           */
+    /* --------------------------------------------------------------------- */
     @Override
     public void hyperlinkUpdate(HyperlinkEvent e) {
         if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
