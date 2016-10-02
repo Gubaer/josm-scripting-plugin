01a511eebafe401ff9ee9c339b9ec4da906cddc3
Gubaer <karl.guggisberg@guggis.ch>
2016-10-02 18:35:51 +0200

    reformat

--- src/org/openstreetmap/josm/plugins/scripting/js/MixinWrapFactory.java
+++ src/org/openstreetmap/josm/plugins/scripting/js/MixinWrapFactory.java
@@ -7,23 +7,27 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.WrapFactory;
 
 /**
- * <p>Custom WrapFactory for the embedded JOSM scripting engine. Wraps java objects and classes
- * in the custom wrappers {@link NativeJavaClassWithJSMixin} and {@link NativeJavaObjectWithJSMixin}
+ * <p>Custom WrapFactory for the embedded JOSM scripting engine. Wraps java
+ * objects and classes in the custom wrappers
+ * {@link NativeJavaClassWithJSMixin} and {@link NativeJavaObjectWithJSMixin}
  * respectively.</p>
  *
  */
 public class MixinWrapFactory extends WrapFactory{
     @SuppressWarnings("unused")
-    static private final Logger logger = Logger.getLogger(MixinWrapFactory.class.getName());
+    static private final Logger logger =
+    Logger.getLogger(MixinWrapFactory.class.getName());
 
     @Override
-    public Scriptable wrapAsJavaObject(Context cx, Scriptable scope, Object javaObject, Class<?> staticType) {
+    public Scriptable wrapAsJavaObject(Context cx, Scriptable scope,
+            Object javaObject, Class<?> staticType) {
         return new NativeJavaObjectWithJSMixin(scope, javaObject, staticType);
     }
 
     @SuppressWarnings("rawtypes")
     @Override
-    public Scriptable wrapJavaClass(Context cx, Scriptable scope, Class javaClass) {
+    public Scriptable wrapJavaClass(Context cx, Scriptable scope,
+            Class javaClass) {
         return new NativeJavaClassWithJSMixin(scope, javaClass);
     }
 }
