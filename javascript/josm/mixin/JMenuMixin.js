b4705123b564495436fdb312f4b10a58697c9789
Gubaer <karl.guggisberg@guggis.ch>
2016-10-07 08:40:22 +0200

    reformat

--- javascript/josm/mixin/JMenuMixin.js
+++ javascript/josm/mixin/JMenuMixin.js
@@ -1,9 +1,9 @@
 (function() {
-	
+
 /**
-* <p>This module is auto-loaded by the scripting plugin and mixed into the 
+* <p>This module is auto-loaded by the scripting plugin and mixed into the
  * native java class {@class javax.swing.JMenu}.</p>
- * 
+ *
  * @module josm/mixin/JMenuMixin
  */
 
@@ -12,14 +12,15 @@ var util = require("josm/util");
 var mixin = {};
 
 mixin.add = function() {
-	util.assert(arguments.length > 0, "Expected > 0 arguments, got {0}", arguments.length);
-	if (arguments.length instanceof JSAction) {
-		var action = arguments[0];
-		action.addToMenu(this, arguments[1]);
-	} else {
-		// invoke the native add method
-		this.$add.apply(this, Array.prototype.slice.call(arguments,0));
-	}
+    util.assert(arguments.length > 0,
+        "Expected > 0 arguments, got {0}", arguments.length);
+    if (arguments.length instanceof JSAction) {
+        var action = arguments[0];
+        action.addToMenu(this, arguments[1]);
+    } else {
+        // invoke the native add method
+        this.$add.apply(this, Array.prototype.slice.call(arguments,0));
+    }
 };
 
 }());
