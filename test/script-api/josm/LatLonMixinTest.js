60f3aa629c425e7bc9515ed8300d1243e0218700
Gubaer <karl.guggisberg@guggis.ch>
2016-10-08 19:39:01 +0200

    reformat

--- test/script-api/josm/LatLonMixinTest.js
+++ test/script-api/josm/LatLonMixinTest.js
@@ -7,85 +7,83 @@ var LatLon    = org.openstreetmap.josm.data.coor.LatLon;
 var suites = [];
 
 suites.push(tu.suite("properties acess",
-	test("lat", function() {
-		var pos = new LatLon(1,2);
-		util.assert(pos.lat == 1, "1- unexpected lat");		
-		util.assert(pos.$lat() == 1, "2 - unexpected lat");
+    test("lat", function() {
+        var pos = new LatLon(1,2);
+        util.assert(pos.lat == 1, "1- unexpected lat");
+        util.assert(pos.$lat() == 1, "2 - unexpected lat");
 
-		tu.expectError("can't set value for 'lat'", function() {
-			pos.lat = 1;
-		});
-	}),
-	
-	test("lon", function() {
-		tu.expectAssertionError("id - set - should fail", function() {
-			var pos = new LatLon(1,2);
-			util.assert(pos.lon == 2, "1- unexpected lon");		
-			util.assert(pos.$lon() == 2, "2 - unexpected lon");
+        tu.expectError("can't set value for 'lat'", function() {
+            pos.lat = 1;
+        });
+    }),
 
-			tu.expectError("can't set value for 'lon'", function() {
-				pos.lon = 1;
-			});
-		});
-	})
+    test("lon", function() {
+        tu.expectAssertionError("id - set - should fail", function() {
+            var pos = new LatLon(1,2);
+            util.assert(pos.lon == 2, "1- unexpected lon");
+            util.assert(pos.$lon() == 2, "2 - unexpected lon");
+
+            tu.expectError("can't set value for 'lon'", function() {
+                pos.lon = 1;
+            });
+        });
+    })
 ));
 
 suites.push(tu.suite("make",
-	test("make - valid object", function() {
-		var pos = LatLon.make({lat: 1, lon: 2});
-		util.assert(pos.lat == 1, "1- unexpected lat");		
-		util.assert(pos.lon == 2, "2 - unexpected lon");	
-	}),
-	test("make - incomplete object", function() {
-		tu.expectError("missing lat", function() {
-			var pos = LatLon.make({lon: 2});
-		});
-		
-		tu.expectError("missing lon", function() {
-			var pos = LatLon.make({lat: 2});
-		});
-	}),
-	test("make - illegal lats", function() {
-		tu.expectError("null", function() {
-			var pos = LatLon.make({lat: null, lon: 2});
-		});
-		
-		tu.expectError("undefined", function() {
-			var pos = LatLon.make({lat: undefined, lon: 2});
-		});
-		
-		tu.expectError("string", function() {
-			var pos = LatLon.make({lat: "1", lon: 2});
-		});
-		
-		tu.expectError("illegal lat", function() {
-			var pos = LatLon.make({lat: 200, lon: 2});
-		});
-	}),
-	test("make - illegal lons", function() {
-		tu.expectError("null", function() {
-			var pos = LatLon.make({lat: 1, lon: null});
-		});
-		
-		tu.expectError("undefined", function() {
-			var pos = LatLon.make({lat: 1, lon: undefined});
-		});
-		
-		tu.expectError("string", function() {
-			var pos = LatLon.make({lat: 1, lon: "2"});
-		});
-		
-		tu.expectError("illegal lat", function() {
-			var pos = LatLon.make({lat: 1, lon: 200});
-		});
-	})
+    test("make - valid object", function() {
+        var pos = LatLon.make({lat: 1, lon: 2});
+        util.assert(pos.lat == 1, "1- unexpected lat");
+        util.assert(pos.lon == 2, "2 - unexpected lon");
+    }),
+    test("make - incomplete object", function() {
+        tu.expectError("missing lat", function() {
+            var pos = LatLon.make({lon: 2});
+        });
+
+        tu.expectError("missing lon", function() {
+            var pos = LatLon.make({lat: 2});
+        });
+    }),
+    test("make - illegal lats", function() {
+        tu.expectError("null", function() {
+            var pos = LatLon.make({lat: null, lon: 2});
+        });
+
+        tu.expectError("undefined", function() {
+            var pos = LatLon.make({lat: undefined, lon: 2});
+        });
+
+        tu.expectError("string", function() {
+            var pos = LatLon.make({lat: "1", lon: 2});
+        });
+
+        tu.expectError("illegal lat", function() {
+            var pos = LatLon.make({lat: 200, lon: 2});
+        });
+    }),
+    test("make - illegal lons", function() {
+        tu.expectError("null", function() {
+            var pos = LatLon.make({lat: 1, lon: null});
+        });
+
+        tu.expectError("undefined", function() {
+            var pos = LatLon.make({lat: 1, lon: undefined});
+        });
+
+        tu.expectError("string", function() {
+            var pos = LatLon.make({lat: 1, lon: "2"});
+        });
+
+        tu.expectError("illegal lat", function() {
+            var pos = LatLon.make({lat: 1, lon: 200});
+        });
+    })
 ));
 
 exports.run = function() {
-	for (var i=0; i<suites.length; i++) {
-		suites[i].run();
-	}
-	
-};
+    for (var i=0; i<suites.length; i++) {
+        suites[i].run();
+    }
 
-		
+};
