456c7c67a40730e9e59fe761eb299e51aea8c194
Gubaer <karl.guggisberg@guggis.ch>
2016-10-09 12:32:51 +0200

    reformat

--- test/script-api/josm/RelationMixinTest.js
+++ test/script-api/josm/RelationMixinTest.js
@@ -28,7 +28,8 @@ suites.push(tu.suite("properties access",
 		util.assert(r.members.length == 0, "should be empty array");
 		
 		r = rb.createProxy(1234);		
-		util.assert(r.members.length == 0, "should be empty array, for proxy relation too");
+		util.assert(r.members.length == 0, 
+			"should be empty array, for proxy relation too");
 		
 		r = rb.withMembers(nb.create(),nb.create()).create(1234);
 		util.assert(r.members.length == 2, "unexpected number of members");
@@ -54,7 +55,8 @@ suites.push(tu.suite("properties access",
 		var r; 
 		
 		r = rb.create(1234);
-		r.members = [nb.create(), rb.member('role.1', nb.create()), wb.create(12345)];
+		r.members = [nb.create(), rb.member('role.1', nb.create()), 
+		             wb.create(12345)];
 		util.assert(r.length == 3, "1 - unexpected number of members");
 		
 		var list = new ArrayList();
@@ -67,14 +69,16 @@ suites.push(tu.suite("properties access",
 		
 		// nulls and undefined are ignored 
 		r = rb.create(1234);
-		r.members = [null, nb.create(), rb.member('role.1', nb.create()), undefined, wb.create(12345)];
+		r.members = [null, nb.create(), rb.member('role.1', nb.create()), 
+		             undefined, wb.create(12345)];
 		util.assert(r.length == 3, "3  - unexpected number of members");
 	})
 ));
 
 suites.push(tu.suite("get and set relation members using [...]",
 	test("get with [..]", function() {
-		var members = [nb.create(), rb.member('role.1', nb.create()), wb.create(12345)];
+		var members = [nb.create(), rb.member('role.1', nb.create()), 
+		               wb.create(12345)];
 		var r = rb.withMembers(members).create(12345);
 		util.assert(r[0].role == "", "unexpected role");
 		util.assert(r[1].member instanceof Node , "unexpected type");
@@ -85,13 +89,15 @@ suites.push(tu.suite("get and set relation members using [...]",
 	}),
 	
 	test("set with [..]", function() {
-		var members = [nb.create(), rb.member('role.1', nb.create()), wb.create(12345)];
+		var members = [nb.create(), rb.member('role.1', nb.create()), 
+		               wb.create(12345)];
 		var r = rb.withMembers(members).create(12345);
 		
 		r[0] = wb.create(1111);
 		util.assert(r[0].member instanceof Way, "should be a way");
 		r[1] = rb.member('role.2', nb.create());
-		util.assert(r[1].role == "role.2", "unexpected role, should be {0}", "role.2");
+		util.assert(r[1].role == "role.2", 
+			"unexpected role, should be {0}", "role.2");
 		r[3] = nb.create();
 		util.assert(r.length == 4, "unexpected length");
 		
@@ -119,7 +125,8 @@ suites.push(tu.suite("get and set relation members using [...]",
 
 suites.push(tu.suite("modified flag",
 	test("modify the members list", function() {
-		var members = [nb.create(), rb.member('role.1', nb.create()), wb.create(12345)];		
+		var members = [nb.create(), rb.member('role.1', nb.create()), 
+		               wb.create(12345)];		
 		var r = rb.withMembers(members).create(12345);
 		util.assert(!r.modified, "1 - should not be modified");
 		
