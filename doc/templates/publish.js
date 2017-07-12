23d07b4a0bd32b67a893ac2225ea81096db87ef6
Gubaer <karl.guggisberg@guggis.ch>
2017-07-12 15:15:02 +0200

    cleanup white space

--- doc/templates/publish.js
+++ doc/templates/publish.js
@@ -1,6 +1,6 @@
 /**
- * publish 
- * 
+ * publish
+ *
  */
 
 var fs   = require("fs-extra"),
@@ -10,92 +10,87 @@ var fs   = require("fs-extra"),
     ViewHelper = require("viewhelper").ViewHelper,
     safeHtmlFilename = require("viewhelper").safeHtmlFilename;
 
-function isString(value) {
-	return typeof value === "string" || value instanceof String;
-}
-
 /**
- * 
+ *
  * @global
  * @param {TAFFY} data - A TaffyDB collection representing
  *                       all the symbols documented in your code.
  * @param {object} opts - An object with options information.
  */
 exports.publish = function(data, opts, tutorials) {
-	
-	var safeHtmlFilename = require("viewhelper").safeHtmlFilename;
 
-	function publishDoclet(doclet, config) {
-		var dir = path.join(
-		        opts.destination, 
-		        config.path 
-		);
-		fs.ensureDirSync(dir);
-		var file = path.join(dir, safeHtmlFilename(doclet.name));
-		var fragment = view.render(config.template, {
-			doclet: doclet,
-		    data: data,
-		    ViewHelper: ViewHelper
-		});
-		var html = view.render('page.tmpl', {
-			title: config.title + " " + doclet.name,
-			paths: {
-				stylesheets: "../../stylesheets/",
-				javascripts: "../../javascript/"
-			},
-			body: fragment,
-			showHeader: false
-		});
-		console.log(config.title + " <" + doclet.name + ">: writing to <" + file + ">");
-		fs.writeFileSync(file, html, "utf8");
-	};
+    function publishDoclet(doclet, config) {
+        var dir = path.join(
+                opts.destination,
+                config.path
+        );
+        fs.ensureDirSync(dir);
+        var file = path.join(dir, safeHtmlFilename(doclet.name));
+        var fragment = view.render(config.template, {
+            doclet: doclet,
+            data: data,
+            ViewHelper: ViewHelper
+        });
+        var html = view.render('page.tmpl', {
+            title: config.title + " " + doclet.name,
+            paths: {
+                stylesheets: "../../stylesheets/",
+                javascripts: "../../javascript/"
+            },
+            body: fragment,
+            showHeader: false
+        });
+        console.log(config.title + " <" + doclet.name + ">: writing to <" 
+                + file + ">");
+        fs.writeFileSync(file, html, "utf8");
+    };
 
-	function publishTOC() {
-		var filepath = path.join(opts.destination, "apitoc.html");
-		fs.ensureDirSync(opts.destination);
-		var fragment = view.render("toc.tmpl", {
-		    data: data,
-		    safeHtmlFilename: safeHtmlFilename 
-		});
-		console.log("TOC: writing to <" + filepath + ">");
-		fs.writeFileSync(filepath, fragment, "utf8");
-	};
+    function publishTOC() {
+        var filepath = path.join(opts.destination, "apitoc.html");
+        fs.ensureDirSync(opts.destination);
+        var fragment = view.render("toc.tmpl", {
+            data: data,
+            safeHtmlFilename: safeHtmlFilename
+        });
+        console.log("TOC: writing to <" + filepath + ">");
+        fs.writeFileSync(filepath, fragment, "utf8");
+    };
 
-	var view = new template.Template(opts.template + "/tmpl");
+    var view = new template.Template(opts.template + "/tmpl");
 
-	data({kind: "mixin"}).each(function(mixin) {
-		publishDoclet(mixin, {
-			title: "Mixin",
-			path: "mixins",
-			template: "type.tmpl"
-		});
-	});	   
-	
-	data({kind: "class"}).each(function(cls) {
-		if (cls.name == cls.memberof) return;
-		publishDoclet(cls, {
-			title: "Class",
-			path: "classes",
-			template: 'type.tmpl'
-		});
-	});
+    data({kind: "mixin"}).each(function(mixin) {
+        publishDoclet(mixin, {
+            title: "Mixin",
+            path: "mixins",
+            template: "type.tmpl"
+        });
+    });
 
-	data({kind: "namespace"}).each(function(cls) {
-		publishDoclet(cls, {
-			title: "Namespace ",
-			path: "namespaces",
-			template: 'type.tmpl'
-		});
-	});	   
-	
-	data({kind: "module"}).each(function(module) {
-		publishDoclet(module, {
-			title: "Module ",
-			path: "modules",
-			template: 'module.tmpl'
-		});;
-	});	   
-	
-	publishTOC();
+    data({kind: "class"}).each(function(cls) {
+        if (cls.name == cls.memberof) return;
+        publishDoclet(cls, {
+            title: "Class",
+            path: "classes",
+            template: 'type.tmpl'
+        });
+    });
+
+    data({kind: "namespace"}).each(function(cls) {
+        publishDoclet(cls, {
+            title: "Namespace ",
+            path: "namespaces",
+            template: 'type.tmpl'
+        });
+    });
+
+    data({kind: "module"}).each(function(module) {
+        publishDoclet(module, {
+            title: "Module ",
+            path: "modules",
+            template: 'module.tmpl'
+        });;
+    });
+
+    publishTOC();
 
 };
--- doc/templates/viewhelper.js
+++ doc/templates/viewhelper.js
@@ -1,78 +1,76 @@
 
 
 var URL_PREFIXES = {
-	"org.openstreetmap.josm": "http://josm.openstreetmap.de/doc/",
-	"java."                 : "http://docs.oracle.com/javase/6/docs/api/",
-	"javax.swing."          : "http://docs.oracle.com/javase/6/docs/api/"
+    "org.openstreetmap.josm": "http://josm.openstreetmap.de/doc/",
+    "java."                 : "http://docs.oracle.com/javase/6/docs/api/",
+    "javax.swing."          : "http://docs.oracle.com/javase/6/docs/api/"
  };
 
 
 function safeHtmlFilename(fn) {
-	return fn.replace(/[^a-zA-Z0-9$\-_\.]/g, "_") + ".html";
+    return fn.replace(/[^a-zA-Z0-9$\-_\.]/g, "_") + ".html";
 }
 
 function docPrefix(type) {
-	 for (var prefix in URL_PREFIXES) {
-		 if (type.indexOf(prefix) == 0) return URL_PREFIXES[prefix];
-	 }
-	 return undefined;
+     for (var prefix in URL_PREFIXES) {
+         if (type.indexOf(prefix) == 0) return URL_PREFIXES[prefix];
+     }
+     return undefined;
  }
 
 function resolveClassReferences(str) {
-	var self = this;
-	if (str == null || str === void 0) return "";
- 	str = str.replace(/(?:\[(.+?)\])?\{@class +(.+?)\}/gi,
-	    function(match, content, longname) {
-			  var fqclassname = longname.replace(/\//g, ".");  
-			  var classname = fqclassname.replace(/.*\.([^\.]+)$/, "$1"); 
-			  content = content || classname;
-			  return self.resolveTypes(fqclassname, content);
-	    }
+    var self = this;
+    if (str == null || str === void 0) return "";
+     str = str.replace(/(?:\[(.+?)\])?\{@class +(.+?)\}/gi,
+        function(match, content, longname) {
+              var fqclassname = longname.replace(/\//g, ".");
+              var classname = fqclassname.replace(/.*\.([^\.]+)$/, "$1");
+              content = content || classname;
+              return self.resolveTypes(fqclassname, content);
+        }
      );
      return str;
 };
 
-function resolveTypes(type, content) {	
-	 //var MessageFormat = java.text.MessageFormat;
+function resolveTypes(type, content) {
     var util = require("util");
-	 if (type == null || type == undefined) return type;
-	 var types = String(type).split(",");
-	 var resolved = [];
-	 for (var i=0; i< types.length; i++){
-		 var type = types[i];
-		 type = type.replace(/^\s+/,"").replace(/\+$/, ""); // trim
-		 var prefix = docPrefix(type);
-		 if (prefix) {
-			 var classname = type.replace(/.*\.([^\.]+)$/, "$1"); 
-			 var url = prefix + type.replace(/\./g, "/") + ".html";
-			 content = content || classname;
-			 //type = MessageFormat.format("<a href=''{0}'' alt=''{1}'' target=''javadoc''>{2}</a>", url, type, content)
-			type = util.format("<a href='%s' alt='%s' target='javadoc'>%s</a>", url, type, content)
-		 } else {
-			 var res = this.data({name: type});
-			 if (res.count() < 1) return type;
-			 content  = content || type;
-			 if (res.first().kind == "class") {
-				 //return MessageFormat.format("<a href=''../classes/{0}'' alt=''{1}''>{2}</a>", safeHtmlFilename(type), type, content);
-				 return util.format("<a href='../classes/%s' alt='%s'>%s</a>", safeHtmlFilename(type), type, content);
-			 } else if (res.first().kind == "mixin") {
-				 //return MessageFormat.format("<a href=''../mixins/{0}'' alt=''{1}''>{2}</a>", safeHtmlFilename(type), type, content);
-				 return util.format("<a href='../mixins/%s' alt='%s'>%s</a>", safeHtmlFilename(type), type, content);
-			 } else if (res.first().kind == "module") {
-				 //return MessageFormat.format("<a href=''../modules/{0}'' alt=''{1}''>{2}</a>", safeHtmlFilename(type), type, content);
-				 return util.format("<a href='../modules/%s' alt='%s'>%s</a>", safeHtmlFilename(type), type, content);
-			 } else if (res.first().kind == "namespace") {
-				 //return MessageFormat.format("<a href=''../namespaces/{0}'' alt=''{1}''>{2}</a>", safeHtmlFilename(type), type, content);
-				 return util.format("<a href='../namespaces/%s' alt='%s'>%s</a>", safeHtmlFilename(type), type, content);
-			 }  else {
-				 return type;
-			 }
-		 }
-		 resolved.push(type);		 
-	 }
-	 return resolved.join("|"); 
+     if (type == null || type == undefined) return type;
+     var types = String(type).split(",");
+     var resolved = [];
+     for (var i=0; i< types.length; i++){
+         var type = types[i];
+         type = type.replace(/^\s+/,"").replace(/\+$/, ""); // trim
+         var prefix = docPrefix(type);
+         if (prefix) {
+             var classname = type.replace(/.*\.([^\.]+)$/, "$1");
+             var url = prefix + type.replace(/\./g, "/") + ".html";
+             content = content || classname;
+            type = util.format("<a href='%s' alt='%s' target='javadoc'>%s</a>",
+                    url, type, content)
+         } else {
+             var res = this.data({name: type});
+             if (res.count() < 1) return type;
+             content  = content || type;
+             if (res.first().kind == "class") {
+                 return util.format("<a href='../classes/%s' alt='%s'>%s</a>",
+                         safeHtmlFilename(type), type, content);
+             } else if (res.first().kind == "mixin") {
+                 return util.format("<a href='../mixins/%s' alt='%s'>%s</a>",
+                         safeHtmlFilename(type), type, content);
+             } else if (res.first().kind == "module") {
+                 return util.format("<a href='../modules/%s' alt='%s'>%s</a>",
+                         safeHtmlFilename(type), type, content);
+             } else if (res.first().kind == "namespace") {
+                 return util.format("<a href='../namespaces/%s' alt='%s'>%s</a>",
+                         safeHtmlFilename(type), type, content);
+             }  else {
+                 return type;
+             }
+         }
+         resolved.push(type);
+     }
+     return resolved.join("|");
 }
-
 
 /**
  * Builds the list of property links as <a ...> elements
@@ -114,20 +112,20 @@ function propertyNames(doclet) {
 /**
  * Build the title string for a doc page of a type (a class, a mixin,
  * a namespace, or a module).
- * 
- * @param doclet the doclet representing the type 
+ *
+ * @param doclet the doclet representing the type
  * @returns {String}
  */
 function buildTitleForType(doclet) {
-	if (doclet.kind == "class") return "Class " + doclet.name
-	else if (doclet.kind == "mixin") return "Mixin " + doclet.name
-	else if (doclet.kind == "namespace") return "Namespace " + doclet.name
-	else if (doclet.kind == "module") return "Module " + doclet.name
-	else return "";
+    if (doclet.kind == "class") return "Class " + doclet.name
+    else if (doclet.kind == "mixin") return "Mixin " + doclet.name
+    else if (doclet.kind == "namespace") return "Namespace " + doclet.name
+    else if (doclet.kind == "module") return "Module " + doclet.name
+    else return "";
 }
 
 function ViewHelper(data) {
-	this.data = data; 
+    this.data = data;
 }
 
 ViewHelper.prototype.resolveClassReferences = resolveClassReferences;
