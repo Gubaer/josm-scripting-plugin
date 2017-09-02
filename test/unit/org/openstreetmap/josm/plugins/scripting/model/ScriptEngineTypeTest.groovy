092c1b758932d3721bc491a19e6038c20d4c6a0c
Gubaer <karl.guggisberg@guggis.ch>
2017-09-02 15:37:47 +0200

    fix whitespace

--- test/unit/org/openstreetmap/josm/plugins/scripting/model/ScriptEngineTypeTest.groovy
+++ test/unit/org/openstreetmap/josm/plugins/scripting/model/ScriptEngineTypeTest.groovy
@@ -6,37 +6,36 @@ import org.openstreetmap.josm.plugins.scripting.model.ScriptEngineDescriptor.Scr
 
 class ScriptEngineTypeTest {
 
-	@Test
-	public void fromLegalPreferenceValue() {
-		def type = ScriptEngineType.fromPreferencesValue("embedded/rhino");
-		assert type == ScriptEngineType.EMBEDDED;
-		
-		type = ScriptEngineType.fromPreferencesValue("plugged/rhino");
-		assert type == ScriptEngineType.PLUGGED;
+    @Test
+    public void fromLegalPreferenceValue() {
+        def type = ScriptEngineType.fromPreferencesValue("embedded/rhino");
+        assert type == ScriptEngineType.EMBEDDED;
 
-		type = ScriptEngineType.fromPreferencesValue("embedded");
-		assert type == ScriptEngineType.EMBEDDED;
-		
-		type = ScriptEngineType.fromPreferencesValue("plugged");
-		assert type == ScriptEngineType.PLUGGED;
-	}
-	
-	@Test
-	public void fromNormalizedPreferenceValue() {
-		def type = ScriptEngineType.fromPreferencesValue("  embedded/rhino  ");
-		assert type == ScriptEngineType.EMBEDDED;
-		
-		type = ScriptEngineType.fromPreferencesValue("  emBeDded/rhino  ");
-		assert type == ScriptEngineType.EMBEDDED;
+        type = ScriptEngineType.fromPreferencesValue("plugged/rhino");
+        assert type == ScriptEngineType.PLUGGED;
 
-		
-		type = ScriptEngineType.fromPreferencesValue("\t plugged/rhino  \t");
-		assert type == ScriptEngineType.PLUGGED;
+        type = ScriptEngineType.fromPreferencesValue("embedded");
+        assert type == ScriptEngineType.EMBEDDED;
 
-		type = ScriptEngineType.fromPreferencesValue("   embedded   ");
-		assert type == ScriptEngineType.EMBEDDED;
-		
-		type = ScriptEngineType.fromPreferencesValue("   plugged  ");
-		assert type == ScriptEngineType.PLUGGED;
-	}
+        type = ScriptEngineType.fromPreferencesValue("plugged");
+        assert type == ScriptEngineType.PLUGGED;
+    }
+
+    @Test
+    public void fromNormalizedPreferenceValue() {
+        def type = ScriptEngineType.fromPreferencesValue("  embedded/rhino  ");
+        assert type == ScriptEngineType.EMBEDDED;
+
+        type = ScriptEngineType.fromPreferencesValue("  emBeDded/rhino  ");
+        assert type == ScriptEngineType.EMBEDDED;
+
+        type = ScriptEngineType.fromPreferencesValue("\t plugged/rhino  \t");
+        assert type == ScriptEngineType.PLUGGED;
+
+        type = ScriptEngineType.fromPreferencesValue("   embedded   ");
+        assert type == ScriptEngineType.EMBEDDED;
+
+        type = ScriptEngineType.fromPreferencesValue("   plugged  ");
+        assert type == ScriptEngineType.PLUGGED;
+    }
 }
