5a1273b9a44a3f41d0fc8fc04fec59562b6c4835
Florian Schäfer <florian@schaeferban.de>
2018-01-05 12:10:19 +0100

    Replace Main.getLayerManager() by MainApplication.getLayerManager()
    The method has been moved and is no longer available in the old location (see https://josm.openstreetmap.de/changeset/13173/josm#file0).

--- scripts/HelloWorld.js
+++ scripts/HelloWorld.js
@@ -1,9 +1,10 @@
 /*
-* HelloWorld.js  -  displays the number of actually open layers 
+* HelloWorld.js  -  displays the number of actually open layers
 */
 
 var JOptionPane = javax.swing.JOptionPane;
 var Main = org.openstreetmap.josm.Main;
+var MainApplication = org.openstreetmap.josm.gui.MainApplication;
 
-var numlayers = Main.getLayerManager().getLayers().size();
+var numlayers = MainApplication.getLayerManager().getLayers().size();
 JOptionPane.showMessageDialog(Main.parent, "[JavaScript] Hello World! You have " + numlayers + " layer(s).");
