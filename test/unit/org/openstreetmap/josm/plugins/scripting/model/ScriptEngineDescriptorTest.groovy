91fe85b2bdc0a1d3cf1be35bf6dc2960a2fc8d4a
Gubaer <karl.guggisberg@guggis.ch>
2019-07-23 10:11:30 +0200

    minor cleanup

--- test/unit/org/openstreetmap/josm/plugins/scripting/model/ScriptEngineDescriptorTest.groovy
+++ test/unit/org/openstreetmap/josm/plugins/scripting/model/ScriptEngineDescriptorTest.groovy
@@ -1,6 +1,5 @@
 package org.openstreetmap.josm.plugins.scripting.model
 
-import static org.junit.Assert.*
 import org.junit.*
 import org.openstreetmap.josm.plugins.scripting.model.ScriptEngineDescriptor.ScriptEngineType
 import org.openstreetmap.josm.data.Preferences
@@ -9,10 +8,10 @@ import org.openstreetmap.josm.spi.preferences.Config
 class ScriptEngineDescriptorTest {
 
 
-    def shouldFail = new GroovyTestCase().&shouldFail;
+    def shouldFail = new GroovyTestCase().&shouldFail
 
     @Test
-    public void createDescriptorForPluggedEngine() {
+    void createDescriptorForPluggedEngine() {
         def sd = new ScriptEngineDescriptor("rhino")
         assert sd.getEngineId() == "rhino"
         assert sd.getEngineType() == ScriptEngineType.PLUGGED
@@ -21,7 +20,7 @@ class ScriptEngineDescriptorTest {
         assert sd.getContentMimeTypes() == []
 
         shouldFail(NullPointerException) {
-            sd = new ScriptEngineDescriptor(null);
+            sd = new ScriptEngineDescriptor(null)
         }
 
         sd = new ScriptEngineDescriptor(ScriptEngineType.PLUGGED, "rhino")
@@ -31,12 +30,12 @@ class ScriptEngineDescriptorTest {
         assert sd.getEngineName() != null
 
         shouldFail(NullPointerException) {
-            sd = new ScriptEngineDescriptor(null, "rhino");
+            sd = new ScriptEngineDescriptor(null, "rhino")
         }
     }
 
     @Test
-    public void createDescriptorForEmbeddedEngine() {
+    void createDescriptorForEmbeddedEngine() {
         def sd = new ScriptEngineDescriptor(ScriptEngineType.EMBEDDED, "rhino")
         assert sd.getEngineId() == "rhino"
         assert sd.getEngineType() == ScriptEngineType.EMBEDDED
@@ -44,7 +43,8 @@ class ScriptEngineDescriptorTest {
         assert sd.getEngineName().empty()
         assert sd.getContentMimeTypes() == []
 
-        sd = new ScriptEngineDescriptor(ScriptEngineType.EMBEDDED, "rhino", "JavaScript", "Mozilla Rhino", "text/javascript")
+        sd = new ScriptEngineDescriptor(ScriptEngineType.EMBEDDED, "rhino",
+                "JavaScript", "Mozilla Rhino", "text/javascript")
         assert sd.getEngineId() == "rhino"
         assert sd.getEngineType() == ScriptEngineType.EMBEDDED
         assert sd.getLanguageName().get() == "JavaScript"
@@ -53,35 +53,36 @@ class ScriptEngineDescriptorTest {
     }
 
     @Test
-    public void buildFromPreferencs_MissingPreference() {
-        def pref = new Preferences();
-        def sd = ScriptEngineDescriptor.buildFromPreferences(pref);
+    void buildFromPreferencs_MissingPreference() {
+        def pref = new Preferences()
+        def sd = ScriptEngineDescriptor.buildFromPreferences(pref)
         assert sd == ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
     }
 
     @Test
-    public void buildFromPreferencs_EmbeddedScriptingEngine() {
-        def pref = new Preferences(Config.getPref());
+    void buildFromPreferencs_EmbeddedScriptingEngine() {
+        def pref = new Preferences(Config.getPref())
         pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE, "embedded/rhino")
-        def sd = ScriptEngineDescriptor.buildFromPreferences(pref);
+        def sd = ScriptEngineDescriptor.buildFromPreferences(pref)
         assert sd != null
         assert sd == ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
     }
 
     @Test
-    public void buildFromPreferencs_UnknownEmbeddedScriptingEngine() {
-        def pref = new Preferences(Config.getPref());
-        pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE, "embedded/noSuchEmbeddedEngine")
-        def sd = ScriptEngineDescriptor.buildFromPreferences(pref);
+    void buildFromPreferencs_UnknownEmbeddedScriptingEngine() {
+        def pref = new Preferences(Config.getPref())
+        pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE,
+                "embedded/noSuchEmbeddedEngine")
+        def sd = ScriptEngineDescriptor.buildFromPreferences(pref)
         assert sd != null
         assert sd == ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
     }
 
     @Test
-    public void buildFromPreferencs_PluggedScriptingEngine() {
-        def pref = new Preferences(Config.getPref());
+    void buildFromPreferencs_PluggedScriptingEngine() {
+        def pref = new Preferences(Config.getPref())
         pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE, "plugged/javascript")
-        def sd = ScriptEngineDescriptor.buildFromPreferences(pref);
+        def sd = ScriptEngineDescriptor.buildFromPreferences(pref)
         assert sd != null
         assert sd != ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
         assert sd.getEngineType() == ScriptEngineType.PLUGGED
@@ -89,17 +90,18 @@ class ScriptEngineDescriptorTest {
     }
 
     @Test
-    public void buildFromPreferencs_UnknownPluggedScriptingEngine() {
+    void buildFromPreferencs_UnknownPluggedScriptingEngine() {
         def pref = new Preferences(Config.getPref());
-        pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE, "plugged/noSuchPluggedEngine")
-        def sd = ScriptEngineDescriptor.buildFromPreferences(pref);
+        pref.put(PreferenceKeys.PREF_KEY_SCRIPTING_ENGINE,
+                "plugged/noSuchPluggedEngine")
+        def sd = ScriptEngineDescriptor.buildFromPreferences(pref)
         assert sd != null
         assert sd == ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
     }
 
     @Test
-    public void isDefault() {
-        def sd = ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE;
+    void isDefault() {
+        def sd = ScriptEngineDescriptor.DEFAULT_SCRIPT_ENGINE
         assert sd.isDefault()
         sd = new ScriptEngineDescriptor(ScriptEngineType.PLUGGED, "groovy")
         assert ! sd.isDefault()
