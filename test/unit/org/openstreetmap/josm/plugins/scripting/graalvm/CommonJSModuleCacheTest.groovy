fc7bc928d566ae70395ce337d21ee266da7d5a9b
Gubaer <karl.guggisberg@guggis.ch>
2019-08-28 11:09:20 +0200

    add unit tests for CommonJSModuleCache

---
+++ test/unit/org/openstreetmap/josm/plugins/scripting/graalvm/CommonJSModuleCacheTest.groovy
@@ -0,0 +1,113 @@
+package org.openstreetmap.josm.plugins.scripting.graalvm
+
+import org.graalvm.polyglot.Context
+import org.graalvm.polyglot.Value
+import org.junit.Test
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertEquals
+
+class CommonJSModuleCacheTest {
+
+    @Test
+    void "singleton should be available"() {
+        def instance = CommonJSModuleCache.getInstance()
+        assertNotNull(instance)
+    }
+
+    @Test
+    void "can remember a module which is then available"() {
+        def moduleUri = new File("/repo/base/dir/my-module.js").toURI()
+        def module = Value.asValue("my message")
+        def context = null
+
+        try {
+            context = Context.create("js")
+            def cache = CommonJSModuleCache.getInstance()
+            cache.remember(moduleUri, module, context)
+            def lookedUpModule = cache.lookup(moduleUri, context)
+            assertTrue(lookedUpModule.isPresent())
+            assertEquals(module.asString(), lookedUpModule.get().asString())
+        } finally {
+            context?.close()
+        }
+    }
+
+    @Test
+    void "module which is not registered should not be available"() {
+        def moduleUri = new File("/repo/base/dir/module3").toURI()
+        def context = null
+
+        try {
+            context = Context.create("js")
+            def cache = CommonJSModuleCache.getInstance()
+            def lookedUpModule = cache.lookup(moduleUri, context)
+            assertFalse(lookedUpModule.isPresent())
+        } finally {
+            context?.close()
+        }
+    }
+
+    @Test
+    void "remembered module should not be available in other context"() {
+        def moduleUri = new File("/repo/base/dir/module2.js").toURI()
+        def module = Value.asValue("module2 message")
+        def context1 = null
+        def context2 = null
+
+        try {
+            context1 = Context.create("js")
+            context2 = Context.create("js")
+            def cache = CommonJSModuleCache.getInstance()
+            cache.remember(moduleUri, module, context1)
+            def lookedUpModule = cache.lookup(moduleUri, context2)
+            assertFalse(lookedUpModule.isPresent())
+        } finally {
+            context1?.close()
+            context2?.close()
+        }
+    }
+
+    @Test
+    void "a module which is removed from the cache can't be looked up"() {
+        def moduleUri = new File("/repo/base/dir/my-module.js").toURI()
+        def module = Value.asValue("my message")
+        def context = null
+
+        try {
+            context = Context.create("js")
+            def cache = CommonJSModuleCache.getInstance()
+            cache.remember(moduleUri, module, context)
+            def lookedUpModule = cache.lookup(moduleUri, context)
+            assertTrue(lookedUpModule.isPresent())
+            assertEquals(module.asString(), lookedUpModule.get().asString())
+            cache.clear(moduleUri, context)
+            lookedUpModule = cache.lookup(moduleUri, context)
+            assertFalse(lookedUpModule.isPresent())
+        } finally {
+            context?.close()
+        }
+    }
+
+    @Test
+    void "a module which is removed from the cache can't be looked up (2)"() {
+        def moduleUri = new File("/repo/base/dir/my-module.js").toURI()
+        def module = Value.asValue("my message")
+        def context = null
+
+        try {
+            context = Context.create("js")
+            def cache = CommonJSModuleCache.getInstance()
+            cache.remember(moduleUri, module, context)
+            def lookedUpModule = cache.lookup(moduleUri, context)
+            assertTrue(lookedUpModule.isPresent())
+            assertEquals(module.asString(), lookedUpModule.get().asString())
+            cache.clear(context)
+            lookedUpModule = cache.lookup(moduleUri, context)
+            assertFalse(lookedUpModule.isPresent())
+        } finally {
+            context?.close()
+        }
+    }
+}
