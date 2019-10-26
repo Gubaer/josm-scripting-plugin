7525461ee3f1fa66a7f17c4d0971fee320fdb3f2
Gubaer <karl.guggisberg@guggis.ch>
2019-10-26 09:12:33 +0200

    cleanup and reorganize

---
+++ src/test/functional/java/org/openstreetmap/josm/plugins/scripting/ScriptEngineSelectionDialogTest.java
@@ -0,0 +1,36 @@
+package org.openstreetmap.josm.plugins.scripting;
+
+import org.openstreetmap.josm.plugins.scripting.fixtures.JOSMFixture;
+import org.openstreetmap.josm.plugins.scripting.model.ScriptEngineDescriptor;
+import org.openstreetmap.josm.plugins.scripting.ui.ScriptEngineSelectionDialog;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * functional, interactive test for {@link ScriptEngineSelectionDialog}
+ */
+public class ScriptEngineSelectionDialogTest extends JFrame {
+
+    ScriptEngineSelectionDialogTest() throws Exception {
+        getContentPane().setLayout(new FlowLayout());
+        setSize(100,100);
+        JButton btn = new JButton("Launch");
+        getContentPane().add(btn);
+        btn.addActionListener(e -> {
+            ScriptEngineDescriptor desc =
+                ScriptEngineSelectionDialog.select(
+                    ScriptEngineSelectionDialogTest.this,null);
+            System.out.println(desc);
+        });
+
+        @SuppressWarnings("unused")
+        final JOSMFixture fixture = new JOSMFixture(true);
+    }
+
+    static public void main(String[] args) throws Exception {
+        ScriptEngineSelectionDialogTest app =
+            new ScriptEngineSelectionDialogTest();
+        app.setVisible(true);
+    }
+}
