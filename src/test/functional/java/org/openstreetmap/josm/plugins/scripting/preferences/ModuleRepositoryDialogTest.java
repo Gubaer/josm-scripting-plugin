b2e37f0cb28fb94885f12bfe46f9167988bfd3d1
Gubaer <karl.guggisberg@guggis.ch>
2019-10-26 09:16:02 +0200

    cleanup and reorganize

---
+++ src/test/functional/java/org/openstreetmap/josm/plugins/scripting/preferences/ModuleRepositoryDialogTest.java
@@ -0,0 +1,30 @@
+package org.openstreetmap.josm.plugins.scripting.preferences;
+
+import org.openstreetmap.josm.plugins.scripting.fixtures.JOSMFixture;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ModuleRepositoryDialogTest extends JFrame {
+
+    ModuleRepositoryDialogTest() throws Exception {
+        getContentPane().setLayout(new FlowLayout());
+        setSize(100,100);
+        JButton btn = new JButton("Launch");
+        getContentPane().add(btn);
+        btn.addActionListener(e -> {
+            ModuleRepositoryDialog dialog =
+                new ModuleRepositoryDialog(ModuleRepositoryDialogTest.this);
+            dialog.setVisible(true);
+        });
+
+        @SuppressWarnings("unused")
+        final JOSMFixture fixture = new JOSMFixture(true);
+    }
+
+    static public void main(String[] args) throws Exception {
+        ModuleRepositoryDialogTest app = new ModuleRepositoryDialogTest();
+        app.setVisible(true);
+    }
+}
