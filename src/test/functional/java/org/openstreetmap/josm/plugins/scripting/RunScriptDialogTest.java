4dd21c74f0818ed7990be5479eda3dbdeff4a8f9
Gubaer <karl.guggisberg@guggis.ch>
2019-10-26 09:05:36 +0200

    cleanup and reorganize

--- src/test/functional/java/org/openstreetmap/josm/plugins/scripting/RunScriptDialogTest.java
+++ src/test/functional/java/org/openstreetmap/josm/plugins/scripting/RunScriptDialogTest.java
@@ -1,36 +1,33 @@
 package org.openstreetmap.josm.plugins.scripting;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
 
 import org.openstreetmap.josm.plugins.scripting.fixtures.JOSMFixture;
 import org.openstreetmap.josm.plugins.scripting.ui.RunScriptDialog;
 
+import javax.swing.*;
+import java.awt.*;
 
+/**
+ * Functional, interactive test for {@link RunScriptDialog}.
+ */
 public class RunScriptDialogTest extends JFrame {
 
     private JOSMFixture fixture;
 
     public RunScriptDialogTest() throws Exception {
-        fixture = new JOSMFixture();
+        fixture = new JOSMFixture(true);
 
         getContentPane().setLayout(new FlowLayout());
         setSize(100,100);
         JButton btn = new JButton("Launch");
         getContentPane().add(btn);
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                RunScriptDialog dialog = new RunScriptDialog(RunScriptDialogTest.this);
-                dialog.setVisible(true);
-            }
+        btn.addActionListener(e -> {
+            RunScriptDialog dialog =
+                new RunScriptDialog(RunScriptDialogTest.this);
+            dialog.setVisible(true);
         });
     }
 
-    static public void main(String args[]) throws Exception {
+    static public void main(String[] args) throws Exception {
         RunScriptDialogTest app = new RunScriptDialogTest();
         app.setVisible(true);
     }
