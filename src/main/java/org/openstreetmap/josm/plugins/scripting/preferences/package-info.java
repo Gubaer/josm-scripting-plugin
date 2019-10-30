90302b504897bd1c35f998463f04ac95a522a45f
Gubaer <karl.guggisberg@guggis.ch>
2019-10-30 18:17:01 +0100

    remove unused code

--- src/main/java/org/openstreetmap/josm/plugins/scripting/preferences/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-    namespace = "urn:org.openstreetmap.josm.plugins.scripting",
-    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-package org.openstreetmap.josm.plugins.scripting.preferences;
