524b87b2ce210ec91f60df437f68c3590467df2b
Gubaer <karl.guggisberg@kacon.ch>
2022-05-13 18:26:45 +0200

    move CSS rules to a resource file

--- src/main/java/org/openstreetmap/josm/plugins/scripting/ui/EditorPaneBuilder.java
+++ src/main/java/org/openstreetmap/josm/plugins/scripting/ui/EditorPaneBuilder.java
@@ -5,10 +5,31 @@ import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.StyleSheet;
 import java.awt.*;
 import java.text.MessageFormat;
+import java.util.logging.Logger;
 
 public class EditorPaneBuilder {
+    static private final String RESOURCE_NAME_STYLE_SHEET =
+        "/css/default-editor-pane.css";
+    static private final Logger logger =
+        Logger.getLogger(EditorPaneBuilder.class.getName());
+
+    private static StyleSheet loadDefaultStyleSheet() {
+        var styleSheet = new StyleSheet();
+        final var styleSheetUri =
+            EditorPaneBuilder.class.getResource(RESOURCE_NAME_STYLE_SHEET);
+        if (styleSheetUri == null) {
+            logger.warning(String.format(
+                "failed to load default CSS style sheet from URL '%s'",
+                styleSheetUri.toString()
+            ));
+            return styleSheet;
+        }
+        styleSheet.importStyleSheet(styleSheetUri);
+        return styleSheet;
+    }
+
     /**
-     * Builds an non-editable {@link JEditorPane} and styles it. Font
+     * Builds a non-editable {@link JEditorPane} and styles it. Font
      * parameters match the {@link UIManager}s font parameters.
      *
      * @return the info pane
@@ -18,11 +39,14 @@ public class EditorPaneBuilder {
             new JEditorPane("text/html", null /* no text */);
         jepInfo.setOpaque(false);
         jepInfo.setEditable(false);
+        final StyleSheet ss = loadDefaultStyleSheet();
+
+        // dynamically build a rule for <strong> using the current
+        // font used in labels
         final Font f = UIManager.getFont("Label.font");
-        final StyleSheet ss = new StyleSheet();
         final String cssRuleFontFamily =
             "font-family: ''{0}'';font-size: {1,number}pt; font-weight: {2}; " +
-            "font-style: {3}";
+            "font-style: {3};";
         final String rule = "strong {" +
             MessageFormat.format(cssRuleFontFamily,
                 f.getName(),
@@ -30,18 +54,7 @@ public class EditorPaneBuilder {
                 "bold",
                 f.isItalic() ? "italic" : "normal") +
                 "}";
-        ss.addRule("a { text-decoration: underline; color: blue}");
-        ss.addRule("code { font-family: monospace }");
-        ss.addRule("p { padding-bottom: 10pt }");
-        ss.addRule(
-            "p.warning {"
-            + "background: #ffcccb;"
-            + "padding: 10pt;"
-            + "border-style: solid;"
-            + "border-color: red;"
-            + "border-width: 2pt;"
-            + "}"
-        );
+        ss.addRule(rule);
         final HTMLEditorKit kit = new HTMLEditorKit();
         kit.setStyleSheet(ss);
         jepInfo.setEditorKit(kit);
