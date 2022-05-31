e11b274c562246f1fb5204fe81db18918b0b99c0
Gubaer <karl.guggisberg@kacon.ch>
2022-05-31 11:47:35 +0200

    add sample in ECMAScript suitable for GraalJS

---
+++ src/main/resources/scripts/HelloWorld.GraalJS.js
@@ -0,0 +1,12 @@
+/*
+* HelloWorld.js  -  displays the number of actually open layers
+*/
+
+const JOptionPane = Java.type('javax.swing.JOptionPane')
+const MainApplication = Java.type('org.openstreetmap.josm.gui.MainApplication')
+
+const numLayers = MainApplication.getLayerManager().getLayers().size()
+JOptionPane.showMessageDialog(
+    MainApplication.getMainFrame(),
+    `[JavaScript] Hello World! You have ${numLayers} layers(s)`
+)
