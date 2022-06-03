05085ba03617a9a0d87fa7ec60a98db59eecf01b
Gubaer <karl.guggisberg@kacon.ch>
2022-06-03 17:13:44 +0200

    add unit tests to require() all available modules

--- src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/graalvm/api/APIV2TestSuite.groovy
+++ src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/graalvm/api/APIV2TestSuite.groovy
@@ -1,10 +1,27 @@
 package org.openstreetmap.josm.plugins.scripting.graalvm.api
 
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openstreetmap.josm.plugins.scripting.graalvm.AbstractGraalVMBasedTest
 import org.openstreetmap.josm.plugins.scripting.graalvm.GraalVMFacadeFactory
+import org.openstreetmap.josm.plugins.scripting.graalvm.IGraalVMFacade
 
 class APIV2TestSuite extends AbstractGraalVMBasedTest {
+
+    private IGraalVMFacade facade
+
+    @BeforeEach
+    void initGraalVMFacade() {
+        facade = GraalVMFacadeFactory.getOrCreateGraalVMFacade()
+    }
+
+    @AfterEach
+    void resetGraalVMFacade() {
+        if (facade != null) {
+            facade.resetContext()
+        }
+    }
 
     @Test
     void 'can run API V2 unit tests'() {
@@ -12,13 +29,24 @@ class APIV2TestSuite extends AbstractGraalVMBasedTest {
         const suite = require('suite')
         suite.fragileRun()
         """
-        def facade= GraalVMFacadeFactory.getOrCreateGraalVMFacade()
-        def graalJSDescriptor = facade.getScriptEngineDescriptors().find {desc ->
-            desc.getLanguageName().filter{it == "JavaScript"}
-        }
-        if (graalJSDescriptor == null) {
-            throw new IllegalStateException("Failed to lookup script engine descriptor for GraalJS")
-        }
+        facade.eval(graalJSDescriptor, src)
+    }
+
+    @Test
+    void 'can require all modules'() {
+        final src = """
+        require('josm')
+        require('clipboard')
+        require('josm/api')
+        require('josm/builder')
+        require('josm/command')
+        require('josm/ds')
+        require('josm/layers')
+        require('josm/scriptingconsole')
+        require('josm/unittest')
+        require('josm/util')
+        require('josm/ui/menu')
+        """
         facade.eval(graalJSDescriptor, src)
     }
 }
