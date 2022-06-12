65c4486bae6132f20c5f67d757dadab734243aab
Gubaer <karl.guggisberg@guggis.ch>
2022-06-12 18:09:10 +0200

    add method startsWithESModuleRepoPathPrefix

--- src/main/java/org/openstreetmap/josm/plugins/scripting/graalvm/esmodule/AbstractESModuleRepository.java
+++ src/main/java/org/openstreetmap/josm/plugins/scripting/graalvm/esmodule/AbstractESModuleRepository.java
@@ -4,12 +4,37 @@ import javax.validation.constraints.NotNull;
 import java.nio.file.Path;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 abstract public class AbstractESModuleRepository implements IESModuleRepository {
     // the unique path prefix for absolute paths that refer to modules in this repo
     private final Path uniquePathPrefix = Path.of("/es-module-repo", UUID.randomUUID().toString());
 
-
+    /**
+     * Replies true if <code>modulePath</code> starts with the prefix for a module path
+     * which refers into an ES Module repository. Such a path has to be absolute. It
+     * has to match the pattern <code>/es-module-repo/&ltuuid&gt</code>.
+     *
+     * @param modulePath the module path
+     * @return true if <code>modulePath</code> starts with the prefix; false, otherwise
+     */
+    static public boolean startsWithESModuleRepoPathPrefix(@NotNull final Path modulePath) {
+        if (modulePath == null) {
+            return false;
+        }
+        if (!modulePath.isAbsolute()
+                || ! (modulePath.getNameCount() >= 2)
+                || ! modulePath.getName(0).toString().equalsIgnoreCase("es-module-repo")) {
+            return false;
+        }
+        var uuid = modulePath.getName(1).toString();
+        try {
+            UUID.fromString(uuid);
+            return true;
+        } catch(IllegalArgumentException e) {
+            return false;
+        }
+    }
 
     /**
      * {@inheritDoc}
---
+++ src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/graalvm/esmodule/AbstractESModuleRepositoryTest.groovy
@@ -0,0 +1,39 @@
+package org.openstreetmap.josm.plugins.scripting.graalvm.esmodule
+
+import org.junit.jupiter.api.Test
+import org.openstreetmap.josm.plugins.scripting.BaseTestCase
+
+import java.nio.file.Path
+
+class AbstractESModuleRepositoryTest extends BaseTestCase{
+
+    @Test
+    void "accepts ES Module path with correct prefix"() {
+        final path = Path.of("/es-module-repo", UUID.randomUUID().toString());
+        assertTrue(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(path));
+    }
+
+    @Test
+    void "rejects illegal ES Module paths with incorrect prefix"() {
+        def path
+
+        // path is null
+        assertFalse(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(null))
+
+        // path is not absolute
+        path = Path.of("foo/bar")
+        assertFalse(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(path))
+
+        // path is too short
+        path = Path.of("/foo")
+        assertFalse(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(path))
+
+        // path doesn't start with 'es-module-repo'
+        path = Path.of("/foo/bar/baz")
+        assertFalse(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(path))
+
+        // second component isn't an UUID
+        path = Path.of("/es-module-repo/foo/baz")
+        assertFalse(AbstractESModuleRepository.startsWithESModuleRepoPathPrefix(path))
+    }
+}
