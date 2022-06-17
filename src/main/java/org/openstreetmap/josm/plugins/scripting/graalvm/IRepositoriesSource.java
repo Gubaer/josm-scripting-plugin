a7280eaa39f619a557c21a4b963b86d5444719b9
Gubaer <karl.guggisberg@kacon.ch>
2022-06-17 13:24:26 +0200

    add doc

--- src/main/java/org/openstreetmap/josm/plugins/scripting/graalvm/IRepositoriesSource.java
+++ src/main/java/org/openstreetmap/josm/plugins/scripting/graalvm/IRepositoriesSource.java
@@ -5,9 +5,24 @@ import javax.validation.constraints.Null;
 import java.net.URI;
 import java.util.List;
 
+/**
+ * A source of module repositories, either CommonJS modules or ES Modules.
+ */
 public interface IRepositoriesSource {
 
+    /**
+     * Replies the list of repository base URIs for the repositories
+     * managed by this source.
+     *
+     * @return the list of base URIs
+     */
     @NotNull List<URI> getRepositories();
 
+    /**
+     * Sets a list of repository base URIs for which this source
+     * provides repositories.
+     *
+     * @param repositories the base URIs
+     */
     void setRepositories(@Null List<URI> repositories);
 }
