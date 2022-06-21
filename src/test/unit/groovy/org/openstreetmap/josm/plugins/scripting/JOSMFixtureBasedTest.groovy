6d2a46f734a16474132f4521901d21738e1a7ce2
Gubaer <karl.guggisberg@kacon.ch>
2022-06-21 10:44:49 +0200

    remove unnecessary import

--- src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/JOSMFixtureBasedTest.groovy
+++ src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/JOSMFixtureBasedTest.groovy
@@ -1,7 +1,6 @@
 package org.openstreetmap.josm.plugins.scripting
 
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.openstreetmap.josm.plugins.scripting.fixtures.JOSMFixture
 
