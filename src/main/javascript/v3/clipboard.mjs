82e8826be20422b2364f2ad99a5c585b84f24340
Gubaer <karl.guggisberg@kacon.ch>
2022-06-23 18:32:45 +0200

    cleanup API V3 module 'clipboard'

--- src/main/javascript/v3/clipboard.mjs
+++ src/main/javascript/v3/clipboard.mjs
@@ -9,35 +9,29 @@
  */
 
 /* global Java */
-const clipboard = {}
-export default clipboard
 
-/**
- * Set or get the clipboard content as text
- *
- * <dl>
- *   <dt><code class="signature">get</code></dt>
- *   <dd class="param-desc">Replies the clipboard content as text or <code>undefined</code>,
- *   if no clipboard content is available or if it can't be converted to a
- *   string.</dd>
- *
- *   <dt><code class="signature">set</code></dt>
- *   <dd class="param-desc">Sets the clipboard content</dd>
- * </dl>
- *
- * @example
- * import clipboard from 'clipboard'
- * // set the clipboard content
- * clipboard.text = 'Hello World!'
- *
- * @property {string} text clipboard content as text
- * @name text
- * @summary Set or get the clipboard content as text
- * @static
- */
-Object.defineProperty(clipboard, 'text', {
-  enumerable: true,
-  get: function () {
+export default class Clipboard {
+  /**
+   * Set or get the clipboard content as text
+   *
+   * <dl>
+   *   <dt><code class="signature">get</code></dt>
+   *   <dd class="param-desc">Replies the clipboard content as text or <code>undefined</code>,
+   *   if no clipboard content is available or if it can't be converted to a
+   *   string.</dd>
+   *
+   *   <dt><code class="signature">set</code></dt>
+   *   <dd class="param-desc">Sets the clipboard content</dd>
+   * </dl>
+   *
+   * @example
+   * import clipboard from 'clipboard'
+   * // set the clipboard content
+   * clipboard.text = 'Hello World!'
+   *
+   * @property {string} text clipboard content as text
+   */
+  static get text() {
     const Toolkit = Java.type('java.awt.Toolkit')
     const DataFlavor = Java.type('java.awt.datatransfer.DataFlavor')
     const transferable = Toolkit.getDefaultToolkit()
@@ -49,9 +43,9 @@ Object.defineProperty(clipboard, 'text', {
     } catch (e) {
     }
     return undefined
-  },
+  }
 
-  set: function (value) {
+  static set text(value) {
     const StringSelection = Java.type('java.awt.datatransfer.StringSelection')
     const Toolkit = Java.type('java.awt.Toolkit')
     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
@@ -59,4 +53,4 @@ Object.defineProperty(clipboard, 'text', {
       null
     )
   }
-})
+}
