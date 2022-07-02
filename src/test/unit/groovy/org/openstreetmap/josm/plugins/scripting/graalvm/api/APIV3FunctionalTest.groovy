0cbed7b891657ddf713a628f0e76ea3999ced2f1
Gubaer <karl.guggisberg@kacon.ch>
2022-07-02 08:11:16 +0200

    cleanup imports in unit test

--- src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/graalvm/api/APIV3FunctionalTest.groovy
+++ src/test/unit/groovy/org/openstreetmap/josm/plugins/scripting/graalvm/api/APIV3FunctionalTest.groovy
@@ -1,6 +1,5 @@
 package org.openstreetmap.josm.plugins.scripting.graalvm.api
 
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openstreetmap.josm.plugins.scripting.graalvm.AbstractGraalVMBasedTest
